e5ce741aed12fea6396d30a522f1466593587e21
deepak <dpak.cherian@gmail.com>
2013-02-19 18:38:59 -0500

    Gradient checking code.
    Signed-off-by: deepak <dpak.cherian@gmail.com>

---
+++ check_horgrad.m
@@ -0,0 +1,43 @@
+Tgrad = diff_cgrid(tgrid,S.temp,1);
+Tx0 = diff(S.temp,1,1)./diff(xrmat,1,1);
+
+Tgrad = squeeze(Tgrad(:,1,:));
+Tx0   = squeeze(Tx0(:,1,:));
+
+dT = abs(Tgrad-Tx0);
+
+h(1) = subplot(131);
+contourf(avg1(squeeze(xrmat(:,1,:)),1)/1000,avg1(squeeze(zrmat(:,1,:)),1),Tgrad,20);
+title('horgrad'); colorbar; cx = caxis;
+
+h(2) = subplot(132);
+contourf(avg1(squeeze(xrmat(:,1,:)),1)/1000,avg1(squeeze(zrmat(:,1,:)),1),Tx0,20);
+title('diff'); colorbar; caxis(cx);
+
+h(3) = subplot(133);
+contourf(avg1(squeeze(xrmat(:,1,:)),1)/1000,avg1(squeeze(zrmat(:,1,:)),1),dT./max(dT(:))*100);
+title('% error'); colorbar;
+linkaxes(h,'xy');
+
+%% check_Hz
+
+% check Hz calculation with vertical derivatives
+Tgradz = avg1(diff_cgrid(tgrid,S.temp,3),3);
+Tz0 = diff(S.temp,1,3)./diff(zrmat,1,3);
+
+Tgradz = squeeze(Tgradz(:,1,:));
+Tz0 = squeeze(Tz0(:,1,:));
+dTz = abs(Tgradz - Tz0);
+
+h(1) = subplot(131);
+contourf(avg1(squeeze(xrmat(:,1,:)),2)/1000,avg1(squeeze(zrmat(:,1,:)),2),Tgradz,20);
+title('diff_cgrid'); colorbar; cx = caxis;
+
+h(2) = subplot(132);
+contourf(avg1(squeeze(xrmat(:,1,:)),2)/1000,avg1(squeeze(zrmat(:,1,:)),2),Tz0,20);
+title('diff'); colorbar; caxis(cx);
+
+h(3) = subplot(133);
+contourf(avg1(squeeze(xrmat(:,1,:)),2)/1000,avg1(squeeze(zrmat(:,1,:)),2),dTz./max(dTz(:))*100);
+title('% error'); colorbar;
+linkaxes(h,'xy');
