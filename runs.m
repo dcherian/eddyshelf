817e6e24b11136712a4df60c3342efa480d23e51
deepak <dpak.cherian@gmail.com>
2014-06-06 17:10:11 -0400

    plots to show binned transport of shelf water

--- runs.m
+++ runs.m
@@ -1188,6 +1188,36 @@ methods
         save([runs.dir '/fluxes.mat'], 'csflux', 'asflux');
     end
 
+    % track where on the shelf the water exported across
+    % the shelfbreak is coming from
+    function [] = track_shelfwater(runs)
+        bins = runs.csflux.west.shelfwater.bins;
+        time = runs.csflux.time/86400;
+
+        % check
+        if any(avg1(bins) > runs.bathy.xsb)
+            ind = find(bins > runs.bathy.xsb, 1, 'first');
+        else
+            ind = length(bins);
+        end
+
+        % in Sv and filtered with above check
+        trans = squeeze(runs.csflux.west.shelfwater.trans(:,1,1: ...
+                                                          ind)/1e6);
+
+        % grid matrices
+        bmat = repmat(bins(1:ind),[length(time) 1])./runs.rrshelf;
+        tmat = repmat(time', [1 size(bmat,2)]);
+
+        figure;
+        pcolorcen(bmat, tmat, trans);
+        ylabel('Time days');
+        xlabel('rossby radii from shelfbreak');
+        colorbar;
+        cblabel('Sv');
+        title(runs.name);
+    end
+
     % comparison plots
     function [] = compare_plot(runs,num)
         eddy = runs.eddy;
