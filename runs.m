b48506648e5b0f63a23d6915c42dc89f78015a1a
deepak <dpak.cherian@gmail.com>
2014-07-01 20:39:40 -0400

    improved plot_shelfvorbudget

--- runs.m
+++ runs.m
@@ -4271,7 +4271,10 @@ methods
 
         figure;
         subplot(2,1,1)
-        plot(time, runs.vorbudget.shelf.rv);
+        [ax,h1,h2] = plotyy(time, runs.vorbudget.shelf.rv, runs.csflux.time/86400, ...
+                            runs.csflux.west.shelf/1e6);
+        set(ax(2), 'XTick', []);
+        ylabel(ax(2), 'Cross-shelfbreak Transport (Sv)')
 
         beautify;
         limx = xlim;
@@ -4282,19 +4285,21 @@ methods
 
         subplot(2,1,2)
         hold all
-        plot(time, runs.vorbudget.shelf.str, ...
-             time, runs.vorbudget.shelf.hadv + runs.vorbudget.shelf.vadv, ...
+        plot(time, runs.vorbudget.shelf.str, 'Color', [0.68 0.85 ...
+                            0.90]);
+        plot(time, runs.vorbudget.shelf.hadv + runs.vorbudget.shelf.vadv, ...
              time, runs.vorbudget.shelf.tilt, ...
              time, runs.vorbudget.shelf.bfric, ...
              time, runs.vorbudget.shelf.beta);
         plot(time, runs.vorbudget.shelf.hadv, 'Color',[1 1 1]*0.6);
         plot(time, runs.vorbudget.shelf.vadv, 'Color',[1 1 1]*0.6);
+        plot(time, smooth(runs.vorbudget.shelf.str, 3), 'Color', [0 0 1]);
         xlim(limx);
         liney(0, [], [1 1 1]*0.5);
         xlabel('Time (days)');
         ylabel('sec^{-2}');
         legend('str', 'adv', 'tilt', 'bfric', 'beta', 'hadv', ...
-               'vadv');
+               'vadv', 'Location', 'NorthWest');
         beautify
     end
 
