ab0f62025317ddf1adb219a3e339c6d8cdcbf664
deepak <dpak.cherian@gmail.com>
2014-12-05 11:30:46 -0500

    neater plots for paper outline

--- @runArray/plot_penetration.m
+++ @runArray/plot_penetration.m
@@ -1,13 +1,13 @@
 % diagnostic plots to show how much eddy penetrates slope
 function [] = plot_penetration(runArray)
-    hfig1 = figure;
-    subplot(2,1,1); hold all;
-    subplot(2,1,2); hold all;
+    hfig1 = []; %figure;
+    %subplot(2,1,1); hold all;
+    %subplot(2,1,2); hold all;
 
     hfig2 = figure;
-    ax1 = subplot(2,2,[1 3]); hold all;
-    ax2 = subplot(2,2,2); hold all;
-    ax3 = subplot(2,2,4); hold all;
+    ax1 = gca; hold all; %subplot(2,2,[1 3]); hold all;
+    ax2 = []; %subplot(2,2,2); hold all;
+    ax3 = []; %subplot(2,2,4); hold all;
 
     if isempty(runArray.filter)
         runArray.filter = 1:runArray.len;
@@ -17,24 +17,27 @@ function [] = plot_penetration(runArray)
         ii = runArray.filter(ff);
 
         run  = runArray.array(ii);
-        %name = getname(runArray, ii);
+        name = getname(runArray, ii);
         %name = [getname(runArray, ii) ' | L_{sl} = ' ...
         %        num2str(run.bathy.L_slope/1000) ' km'];
         %name = [getname(runArray, ii) ' | S_\alpha = ' ...
         %        num2str(run.bathy.S_sl)];
-        name = [' H_{sb}/H_{eddy} = ' ...
-                num2str(run.bathy.hsb./run.eddy.Lgauss(1))];
+        %name = [' H_{sb}/H_{eddy} = ' ...
+        %        num2str(run.bathy.hsb./run.eddy.Lgauss(1))];
 
         ndtime = run.eddy.t * 86400 / run.csflux.tscale;
         tinds = vecfind(ndtime, [0.5:0.5:(max(ndtime))]);
 
-        figure(hfig1);
-        subplot(2,1,1);
-        hgplt = plot(ndtime, run.eddy.vor.ne/1000 - run.bathy.xsb/1000);
-        addlegend(hgplt, name);
+        if ~isempty(hfig1)
+            figure(hfig1);
+            subplot(2,1,1);
+            hgplt = plot(ndtime, run.eddy.vor.ne/1000 - run.bathy.xsb/1000);
+            addlegend(hgplt, name);
 
-        subplot(2,1,2);
-        plot(ndtime, run.eddy.vor.se/1000 - run.bathy.xsb/1000);
+            subplot(2,1,2);
+            plot(ndtime, run.eddy.vor.se/1000 - run.bathy.xsb/ ...
+                 1000);
+        end
 
         figure(hfig2)
         axes(ax1)
@@ -46,22 +49,27 @@ function [] = plot_penetration(runArray)
         text(x(tinds), y(tinds), ...
              cellstr(num2str(ndtime(tinds)', 2)));
 
-        axes(ax2)
-        hgplt = plot(ndtime, run.eddy.Lgauss./run.eddy.hcen');
-
-        axes(ax3)
-        hgplt = plot(ndtime, run.eddy.Lgauss);
+        if ~isempty(ax2)
+            axes(ax2)
+            hgplt = plot(ndtime, run.eddy.Lgauss./run.eddy.hcen');
+        end
+        if ~isempty(ax3)
+            axes(ax3)
+            hgplt = plot(ndtime, run.eddy.Lgauss);
+        end
     end
 
     fontSize = [16 16 18];
-    figure(hfig1);
-    subplot(2,1,1)
-    ylabel('Northern edge - Y_{sb}');
-    beautify(fontSize);
-    subplot(2,1,2)
-    ylabel('Southern edge - Y_{sb}');
-    liney(0);
-    beautify(fontSize);
+    if ~isempty(hfig1)
+        figure(hfig1);
+        subplot(2,1,1)
+        ylabel('Northern edge - Y_{sb}');
+        beautify(fontSize);
+        subplot(2,1,2)
+        ylabel('Southern edge - Y_{sb}');
+        liney(0);
+        beautify(fontSize);
+    end
 
     figure(hfig2)
     axes(ax1)
@@ -72,13 +80,20 @@ function [] = plot_penetration(runArray)
     liney(1);
     xlabel('(X-X_0)/(deformation radius)');
     beautify(fontSize);
-    axes(ax2)
-    ylabel('Eddy vertical scale / water depth');
-    ylim([0 0.6]);
-    xlabel('Non-dimensional time');
-    beautify;
-    axes(ax3);
-    ylabel('Eddy vertical scale (m)');
-    beautify;
 
+    hlegend = legend;
+    set(hlegend, 'box', 'off');
+
+    if ~isempty(ax2)
+        axes(ax2)
+        ylabel('Eddy vertical scale / water depth');
+        ylim([0 0.6]);
+        xlabel('Non-dimensional time');
+        beautify;
+    end
+    if ~isempty(ax3)
+        axes(ax3);
+        ylabel('Eddy vertical scale (m)');
+        beautify;
+    end
 end
