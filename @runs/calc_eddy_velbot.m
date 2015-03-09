be1a2c722cf497e2515b9ef17ac65c4ba41550f2
deepak <dpak.cherian@gmail.com>
2015-03-09 11:41:41 -0400

    added debugging code to calc_eddy_velbot

--- @runs/calc_eddy_velbot.m
+++ @runs/calc_eddy_velbot.m
@@ -7,26 +7,39 @@ function [] = calc_eddy_velbot(runs)
     % get bottom velocity scale
     u = runs.ubot;
     v = runs.vbot;
+    %if size(vel,3) > length(runs.eddy.V)
+
+    debug = 0;
 
     vel = sqrt(avg1(u(:,2:end-1,:), 1).^2 + ...
                avg1(v(2:end-1,:,:), 2).^2);
-    %if size(vel,3) > length(runs.eddy.V)
-    runs.eddy.Vb = squeeze(max(max(vel .* runs.eddy.vormask,[], 1), ...
+
+    %vel = avg1(u(:,2:end-1,:),1);
+    runs.eddy.Vb = squeeze(max(max(vel .* runs.eddy.mask,[], 1), ...
                                [], 2))';
 
-    %eddbot = dc_roms_read_data(runs.dir, runs.eddname, [], {'z' 1 ...
-    %                    1}, [], runs.rgrid, 'his', 'single') > runs.eddy_thresh;
-    %Vb2 = squeeze(max(max(vel .* eddbot(2:end-1,2:end-1,:),[], 1), ...
-    %                           [], 2))';
+    if debug
+        eddbot = dc_roms_read_data(runs.dir, runs.eddname, [], {'z' 1 ...
+                            1}, [], runs.rgrid, 'his', 'single') > runs.eddy_thresh;
+        rhobot = dc_roms_read_data(runs.dir, runs.eddname, [], {'z' 1 ...
+                            1}, [], runs.rgrid, 'his', 'single') > runs.eddy_thresh;
+
+        Vb2 = squeeze(max(max(vel .* runs.eddy.mask .* eddbot(2:end-1,2:end-1,:),[], 1), ...
+                          [], 2))';
+
+        Vb3 = squeeze(max(max(vel .* runs.eddy.mask .* rhobot(2:end-1,2:end-1,:),[], 1), ...
+                          [], 2))';
+
+        figure;
+        hold all;
+        plot(runs.eddy.Vb);
+        plot(Vb2);
+        plot(Vb3);
+        legend('eddy mask', 'eddy dye', 'rho mask');
+    end
 
     runs.eddy.hash = githash([mfilename('fullpath') '.m']);
 
     eddy = runs.eddy;
     save([runs.dir '/eddytrack.mat'], 'eddy');
-
-        %else
-        %Ub = squeeze(max(max(bsxfun(@times, ...
-        %                            vel(:,2:end-1,:), ...
-        %                            runs.eddy.vormask), [], 1), [], 2));
-        %end
 end
