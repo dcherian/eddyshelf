20fcbf3e09c6b335ae6334bc0e2d51426ee6e1ec
deepak <dpak.cherian@gmail.com>
2015-04-05 19:55:31 -0400

    update to make plot_eddytrack more configurable

--- @runs/plot_eddytrack.m
+++ @runs/plot_eddytrack.m
@@ -8,21 +8,26 @@ function [] = plot_eddytrack(runs)
         y0 = runs.eddy.my(1);
     end
 
-    plotx = (runs.eddy.mx - x0)/runs.rrdeep;
-    ploty = (runs.eddy.my - y0)/runs.rrdeep;
+    xnorm = runs.eddy.vor.dia(1)/2;
+    ynorm = runs.eddy.vor.dia(1)/2;
+
+    plotx = (runs.eddy.mx - x0)/xnorm;
+    ploty = (runs.eddy.my - y0)/ynorm;
 
     figure;
     insertAnnotation('runs.plot_eddytrack');
     plot(plotx, ploty);
+    xlabel('Along isobath distance / Initial radius');
+    ylabel('Cross isobath dist / Initial radius');
     hold all
 
     %limy = ylim; ylim([-2 max(limy)]);
 
     % telescoping line
-    %liney((runs.rgrid.y_rho([runs.params.grid.iyp],1) - runs.bathy.xsb)/runs.rrdeep, ...
+    %liney((runs.rgrid.y_rho([runs.params.grid.iyp],1) - runs.bathy.xsb)/ynorm, ...
     %      'telescope');
     %linex((runs.rgrid.x_rho(1, [runs.params.grid.ixn ...
-    %                    runs.params.grid.ixp]) - runs.eddy.mx(1))/runs.rrdeep, ...
+    %                    runs.params.grid.ixp]) - runs.eddy.mx(1))/xnorm, ...
     %      'telescope');
 
     % sponge line
@@ -33,16 +38,16 @@ function [] = plot_eddytrack(runs)
     ix1 = find(runs.sponge(1:xx, yy) == 1, 1, 'last');
     ix2 = find(runs.sponge(xx:end, yy) == 1, 1, 'first') + xx;
 
-    liney((runs.rgrid.y_rho([iy1 iy2], 1) - y0)/runs.rrdeep, ...
+    liney((runs.rgrid.y_rho([iy1 iy2], 1) - y0)/ynorm, ...
           'sponge');
-    linex((runs.rgrid.x_rho(1, [ix1 ix2]) - x0)/runs.rrdeep, ...
+    linex((runs.rgrid.x_rho(1, [ix1 ix2]) - x0)/xnorm, ...
           'sponge');
 
     if runs.bathy.axis == 'y'
-        liney((runs.bathy.xsl - y0)/runs.rrdeep, ...
+        liney((runs.bathy.xsl - y0)/ynorm, ...
               'slopebreak');
     else
-        linex((runs.bathy.xsl - x0)/runs.rrdeep, ...
+        linex((runs.bathy.xsl - x0)/xnorm, ...
               'slopebreak');
     end
 
