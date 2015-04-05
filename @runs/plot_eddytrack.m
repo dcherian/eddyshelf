f40462900518f505436aab2d8bd33bc298228eb5
deepak <dpak.cherian@gmail.com>
2015-04-05 13:52:52 -0400

    updates to plot_eddytrack. Use fit_traj instead of locate_resistance. Support for NS isobaths

--- @runs/plot_eddytrack.m
+++ @runs/plot_eddytrack.m
@@ -1,21 +1,29 @@
 function [] = plot_eddytrack(runs)
 
-    plotx = (runs.eddy.mx - runs.eddy.mx(1))/runs.rrdeep;
-    ploty = (runs.eddy.my - runs.bathy.xsb)/runs.rrdeep;
+    if runs.bathy.axis == 'y'
+        x0 = runs.eddy.mx(1);
+        y0 = runs.bathy.xsb;
+    else
+        x0 = runs.bathy.xsb;
+        y0 = runs.eddy.my(1);
+    end
+
+    plotx = (runs.eddy.mx - x0)/runs.rrdeep;
+    ploty = (runs.eddy.my - y0)/runs.rrdeep;
 
     figure;
-    insertAnnotation('runArray.plot_eddytrack');
+    insertAnnotation('runs.plot_eddytrack');
     plot(plotx, ploty);
     hold all
 
-    limy = ylim; ylim([-2 max(limy)]);
+    %limy = ylim; ylim([-2 max(limy)]);
 
     % telescoping line
-    liney((runs.rgrid.y_rho([runs.params.grid.iyp],1) - runs.bathy.xsb)/runs.rrdeep, ...
-          'telescope');
-    linex((runs.rgrid.x_rho(1, [runs.params.grid.ixn ...
-                        runs.params.grid.ixp]) - runs.eddy.mx(1))/runs.rrdeep, ...
-          'telescope');
+    %liney((runs.rgrid.y_rho([runs.params.grid.iyp],1) - runs.bathy.xsb)/runs.rrdeep, ...
+    %      'telescope');
+    %linex((runs.rgrid.x_rho(1, [runs.params.grid.ixn ...
+    %                    runs.params.grid.ixp]) - runs.eddy.mx(1))/runs.rrdeep, ...
+    %      'telescope');
 
     % sponge line
     sz = ceil(size(runs.sponge)./2);
@@ -25,15 +33,22 @@ function [] = plot_eddytrack(runs)
     ix1 = find(runs.sponge(1:xx, yy) == 1, 1, 'last');
     ix2 = find(runs.sponge(xx:end, yy) == 1, 1, 'first') + xx;
 
-    liney((runs.rgrid.y_rho([iy1 iy2], 1) - runs.bathy.xsb)/runs.rrdeep, ...
+    liney((runs.rgrid.y_rho([iy1 iy2], 1) - y0)/runs.rrdeep, ...
           'sponge');
-    linex((runs.rgrid.x_rho(1, [ix1 ix2]) - runs.eddy.mx(1))/runs.rrdeep, ...
+    linex((runs.rgrid.x_rho(1, [ix1 ix2]) - x0)/runs.rrdeep, ...
           'sponge');
 
-    liney((runs.bathy.xsl - runs.bathy.xsb)/runs.rrdeep, ...
-          'slopebreak');
+    if runs.bathy.axis == 'y'
+        liney((runs.bathy.xsl - y0)/runs.rrdeep, ...
+              'slopebreak');
+    else
+        linex((runs.bathy.xsl - x0)/runs.rrdeep, ...
+              'slopebreak');
+    end
 
-    [~,~,tind] = runs.locate_resistance;
+    %[~,~,tind] = runs.locate_resistance;
+    runs.fit_traj(1);
+    tind = runs.traj.tind;
     plot(plotx(tind), ploty(tind), 'x', 'MarkerSize', 12);
 
     liney(0);
