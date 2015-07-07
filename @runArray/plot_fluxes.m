9029421ab7a2c6779ed00dd131fafa32e6ee7fed
deepak <dpak.cherian@gmail.com>
2015-07-07 13:10:03 -0400

    plot_fluxes now plots along-shelf streamer structure

--- @runArray/plot_fluxes.m
+++ @runArray/plot_fluxes.m
@@ -1,21 +1,20 @@
 function [] = plot_fluxes(runArray)
-    hfig1 = figure;
-    subplot(2,1,1); hold all
-    subplot(2,1,2); hold all
 
-    hfig2 = figure;
-    hold all
+    hfig1 = figure; subplot(2,1,1); hold all; % shelf water flux time series
+    subplot(2,1,2); hold all;
+
+    hfig2 = figure; hold all % vertical structure / baroclinicity
     % subplot(2,2,1); hold all
     % subplot(2,2,2); hold all
     % subplot(2,2,3); hold all
     % subplot(2,2,4); hold all
 
-    hfig3 = figure;
-    hold all;
+    hfig3 = []; %figure; hold all; % envelope
 
-    hfig4 = figure;
-    subplot(2,1,1); hold all
-    subplot(2,1,2); hold all
+    hfig4 = []; %figure; subplot(2,1,1); hold all; % eddy water flux
+                %subplot(2,1,2); hold all
+
+    hfig5 = figure; hold on;% along shelf structure
 
     if isempty(runArray.filter)
         runArray.filter = 1:runArray.len;
@@ -46,117 +45,179 @@ function [] = plot_fluxes(runArray)
 
         ndtime = run.ndtime; %run.csflux.time/run.tscale
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SHELF WATER
-        figure(hfig1)
-        subplot(2,1,1)
-        hgplt1(ff) = plot(ndtime(1:end-2), ...
-                     smooth((run.csflux.west.shelf(1:end-2, ...
-                                                   1))/fluxscl, 3));
+        if ~isempty(hfig1)
+            figure(hfig1)
+            subplot(2,1,1)
+            hgplt1(ff) = plot(ndtime(1:end-2), ...
+                              smooth((run.csflux.west.shelf(1:end-2, ...
+                                                            1))/fluxscl, 3));
 
-        subplot(2,1,2)
-        plot(ndtime, ...
-             run.csflux.west.itrans.shelf(:,1)/transscl);
+            subplot(2,1,2)
+            plot(ndtime, ...
+                 run.csflux.west.itrans.shelf(:,1)/transscl);
 
-        % total transport
-        ttrans = max(abs(run.csflux.west.itrans.shelf(:,1)));
-        figure(hfig2)
-        try
-            % find location of center at t=1.5 (arbitrary
-            % choice)
-            %                    xnd = (run.csflux.west.shelfwater.bins)./run.rrshelf;
-            %                    subplot(2,2,1)
-            %hgplt = plot(xnd, run.csflux.west.shelfwater.itrans);
-
-            %subplot(2,2,2)
-            %plot(xnd, run.csflux.west.shelfwater.itrans ...
-            %             ./ttrans);
-
-            %subplot(2,2,3)
-            %plot(run.csflux.west.shelfwater.vertitrans, ...
-            %     run.csflux.west.shelfwater.vertbins);
-
-            %subplot(2,2,4)
-            profile = ...
-                run.csflux.west.shelfwater.vertitrans(:,1)./ ...
-                ttrans;
-            zvec = run.csflux.west.shelfwater.vertbins(:,1) ./ ...
-                   run.bathy.hsb;
-            bc = baroclinicity(zvec, profile);
-            hgplt2(ff) = plot(profile, zvec);
-            names2{ff} =  [names{ff} ' | bc = ' num2str(bc,'%.3f')];
-        catch ME
-            keyboard;
-            disp(ME)
+            % total transport
+            ttrans = max(abs(run.csflux.west.itrans.shelf(:,1)));
         end
 
-        figure(hfig3)
-        % change from envelope to depth
-        env = run.csflux.west.shelfwater.envelope;
-        env(isnan(env)) = max(env);
-        ind = vecfind(run.rgrid.y_rho(:,1), env);
-        metric = run.bathy.h(1,ind)./run.bathy.hsb .* ...
-                 (1+run.rgrid.f(run.bathy.isb,1)./run.rgrid.f(ind,1))';
+        %%%%%% BAROCLINICITY
+        if ~isempty(hfig2)
+            figure(hfig2)
+            try
+                % find location of center at t=1.5 (arbitrary
+                % choice)
+                %                    xnd = (run.csflux.west.shelfwater.bins)./run.rrshelf;
+                %                    subplot(2,2,1)
+                %hgplt = plot(xnd, run.csflux.west.shelfwater.itrans);
 
-        hgplt3(ff) = plot(ndtime, ...
-                          (run.bathy.xsb - env)./run.rrshelf);
+                %subplot(2,2,2)
+                %plot(xnd, run.csflux.west.shelfwater.itrans ...
+                %             ./ttrans);
+
+                %subplot(2,2,3)
+                %plot(run.csflux.west.shelfwater.vertitrans, ...
+                %     run.csflux.west.shelfwater.vertbins);
+
+                %subplot(2,2,4)
+                profile = ...
+                    run.csflux.west.shelfwater.vertitrans(:,1)./ ...
+                    ttrans;
+                zvec = run.csflux.west.shelfwater.vertbins(:,1) ./ ...
+                       run.bathy.hsb;
+                bc = baroclinicity(zvec, profile);
+                hgplt2(ff) = plot(profile, zvec);
+                names2{ff} =  [names{ff} ' | bc = ' num2str(bc,'%.3f')];
+            catch ME
+                keyboard;
+                disp(ME)
+            end
+        end
+
+        %%%%%%%%%%%%% ENVELOPE
+        if ~isempty(hfig3)
+            figure(hfig3)
+            % change from envelope to depth
+            env = run.csflux.west.shelfwater.envelope;
+            env(isnan(env)) = max(env);
+            ind = vecfind(run.rgrid.y_rho(:,1), env);
+            metric = run.bathy.h(1,ind)./run.bathy.hsb .* ...
+                     (1+run.rgrid.f(run.bathy.isb,1)./run.rgrid.f(ind,1))';
+
+            hgplt3(ff) = plot(ndtime, ...
+                              (run.bathy.xsb - env)./run.rrdeep);
+        end
 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% EDDY WATER
-        figure(hfig4)
-        subplot(2,1,1)
-        hgplt4(ff) = plot(ndtime(1:end-2), ...
-                     smooth((run.csflux.east.eddy(1:end-2, ...
-                                                  1))/fluxscl, 3));
+        if ~isempty(hfig4)
+            figure(hfig4)
+            subplot(2,1,1)
+            hgplt4(ff) = plot(ndtime(1:end-2), ...
+                              smooth((run.csflux.east.eddy(1:end-2, ...
+                                                           1))/fluxscl, 3));
 
-        subplot(2,1,2)
-        plot(ndtime, ...
-             run.csflux.east.itrans.eddy(:,1)/transscl);
+            subplot(2,1,2)
+            plot(ndtime, ...
+                 run.csflux.east.itrans.eddy(:,1)/transscl);
+        end
+
+        %%%%%%%%%%%%%%%%%%%%% ALONG SHELF STRUCTURE
+        if ~isempty(hfig5)
+            figure(hfig5)
+            tind = run.csflux.tscaleind;
+            matrix = run.csflux.shelfxt(:,:,1);
+            xr = run.rgrid.x_rho(1,:)';
+            nt = size(matrix,2);
+            Lx = run.eddy.vor.dia(1);
+
+            % x-grid to interpolate on to
+            dx = bsxfun(@minus, xr, run.eddy.mx);
+            xmax = max(abs(dx(:)));
+            xi = -1 * xmax: 1000 : xmax;
+
+            mati = nan([length(xi) nt]);
+            for tt = [1:nt]
+                xvec = run.rgrid.x_rho(1,2:end-1)' - run.eddy.mx(tt);
+                mati(:,tt) = interp1(xvec, matrix(:,tt), xi);
+            end
+            %pcolorcen(xi, [1:nt], mati');
+            % integrate in time
+            shelfx = trapz(run.eddy.t, mati./ttrans, 2);
+            hgplt5(ff) = plot(xi./Lx, shelfx);
+        end
     end
 
-    figure(hfig1)
-    subplot(2,1,1)
-    ylabel('Flux of shelf water (Sv)');
-    liney(0, [], [1 1 1]*0.75);
-    subplot(2,1,2)
-    ylabel('Total volume transported');
-    xlabel('Non-dimensional time');
-    legend(hgplt1, names);
+    pause(1); drawnow;
+    if ~isempty(hfig1)
+        figure(hfig1)
+        subplot(2,1,1)
+        ylabel('Flux of shelf water (Sv)');
+        liney(0, [], [1 1 1]*0.75);
+        beautify;
 
-    figure(hfig2)
-    %subplot(2,2,1)
-    %ylabel('Total volume transported (m^3)');
-    %xlabel('cs location (km)');
+        subplot(2,1,2)
+        ylabel('Total volume transported');
+        xlabel('Non-dimensional time');
+        legend(hgplt1, names);
+        beautify;
+    end
 
-    %subplot(2,2,2)
-    %limy = ylim;
-    %ldefbt = sqrt(9.81 * run.bathy.hsb)/run.params.phys.f0;
-    %xnd = xnd .* run.rrshelf ./ ldefbt;
-    %plot(xnd(2:end-1), 1./xnd(2:end-1).^2, 'color', [1 1 ...
-    %                    1]*1);
-    %ylim(limy)
-    %ylabel('Normalized volume transported');
-    %xlabel('cs location / shelfbreak rossby radius');
+    if ~isempty(hfig2)
+        figure(hfig2)
+        %subplot(2,2,1)
+        %ylabel('Total volume transported (m^3)');
+        %xlabel('cs location (km)');
 
-    %subplot(2,2,3)
-    %xlabel('Total volume transported (m^3)');
-    %ylabel('Vertical bin (m)');
+        %subplot(2,2,2)
+        %limy = ylim;
+        %ldefbt = sqrt(9.81 * run.bathy.hsb)/run.params.phys.f0;
+        %xnd = xnd .* run.rrshelf ./ ldefbt;
+        %plot(xnd(2:end-1), 1./xnd(2:end-1).^2, 'color', [1 1 ...
+        %                    1]*1);
+        %ylim(limy)
+        %ylabel('Normalized volume transported');
+        %xlabel('cs location / shelfbreak rossby radius');
 
-    %subplot(2,2,4)
-    ylim([-1 0]);
-    limx = xlim;
-    xlim([0 limx(2)]);
-    xlabel('Normalized volume transported');
-    ylabel('Vertical bin / Shelfbreak depth');
-    legend(hgplt2, names2, 'Location', 'SouthWest');
+        %subplot(2,2,3)
+        %xlabel('Total volume transported (m^3)');
+        %ylabel('Vertical bin (m)');
 
-    figure(hfig3)
-    xlabel('Non-dimensional time');
-    ylabel('isobath most on-shore source of water / h_{sb}');
-    legend(hgplt3, names);
+        %subplot(2,2,4)
+        ylim([-1 0]);
+        limx = xlim;
+        xlim([0 limx(2)]);
+        xlabel('Normalized volume transported');
+        ylabel('Vertical bin / Shelfbreak depth');
+        legend(hgplt2, names2, 'Location', 'SouthWest');
+        beautify;
+    end
 
-    figure(hfig4)
-    subplot(2,1,1);
-    ylabel('Flux of eddy water (Sv)');
-    legend(hgplt4, names);
-    subplot(2,1,2);
-    ylabel('Total volume transported (m^3)');
-    xlabel('Non-dimensional time');
+    if ~isempty(hfig3)
+        figure(hfig3)
+        xlabel('Non-dimensional time');
+        ylabel('isobath most on-shore source of water / h_{sb}');
+        legend(hgplt3, names);
+        beautify;
+    end
+
+    if ~isempty(hfig4)
+        figure(hfig4)
+        subplot(2,1,1);
+        ylabel('Flux of eddy water (Sv)');
+        legend(hgplt4, names);
+        beautify;
+
+        subplot(2,1,2);
+        ylabel('Total volume transported (m^3)');
+        xlabel('Non-dimensional time');
+        beautify;
+    end
+
+    if ~isempty(hfig5)
+        figure(hfig5)
+        xlabel('(X - X_e)/L_x');
+        ylabel('Normalized flux');
+        linex([-1 0 1]); liney(0);
+        legend(hgplt5, names);
+        beautify;
+    end
 end
