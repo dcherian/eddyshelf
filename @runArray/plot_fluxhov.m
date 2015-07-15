28529fc30c7d492b0c9430c8f4064a74f9e12137
deepak <dpak.cherian@gmail.com>
2015-07-15 14:47:00 -0400

    Bugfix in plot_fluxhov. Also use insertAnnotation.

--- @runArray/plot_fluxhov.m
+++ @runArray/plot_fluxhov.m
@@ -1,5 +1,6 @@
 % hovmoeller diagrams of cross-shelfbreak flux
 function [] = plot_fluxhov(runArray, axis)
+
     if ~exist('axis', 'var') || isempty(axis)
         axis = 'z';
     end
@@ -16,29 +17,30 @@ function [] = plot_fluxhov(runArray, axis)
 
         figure;
         hold on
+        insertAnnotation('runArray.plot_fluxhov')
 
         if axis == 'x'
-            matrix = run.csflux.shelfxt;
+            matrix = run.csflux.shelfxt(:,:,1);
             tmat = repmat(run.csflux.time/86400, [size(matrix, 1) 1]);
             xmat = repmat(run.rgrid.x_rho(1,2:end-1)'/1000, ...
                           [1 size(tmat, 2)]);
         else
-            matrix = run.csflux.shelfzt;
+            matrix = run.csflux.shelfzt(:,:,1);
             tmat = repmat(run.csflux.time/run.tscale, [size(matrix, 1) 1]);
             xmat = repmat(run.rgrid.z_r(:,run.bathy.isb,1), ...
                           [1 size(tmat, 2)]);
             xmat = xmat./max(abs(xmat(:)));
         end
 
-        pcolorcen(tmat, xmat, matrix./max(abs(matrix(:))));
+        pcolorcen(xmat, tmat, matrix);
         colormap(cmap);
         colorbar; center_colorbar;
         title(run.name);
 
         if axis == 'x'
-            plot(run.eddy.vor.cx/1000, run.eddy.t);
-            plot(run.eddy.vor.ee/1000, run.eddy.t);
-            plot(run.eddy.vor.we/1000, run.eddy.t);
+            plot(run.eddy.vor.cx/1000, run.eddy.t, 'k');
+            plot(run.eddy.vor.ee/1000, run.eddy.t, 'k');
+            plot(run.eddy.vor.we/1000, run.eddy.t, 'k');
             ylabel('Time (days)');
             xlabel('X (km)');
         else
