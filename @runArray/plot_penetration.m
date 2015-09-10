57d9ceec7d8241c0834388db314f05ce2349a784
deepak <dpak.cherian@gmail.com>
2015-09-10 10:39:50 -0400

    Mark time of max flux on penetration plot

--- @runArray/plot_penetration.m
+++ @runArray/plot_penetration.m
@@ -74,7 +74,8 @@ function [] = plot_penetration(runArray, ax, choices)
         ndtime = run.eddy.t * 86400 / run.eddy.turnover;
         if mark_timestamp
             % tinds = vecfind(run.eddy.t, [dt:dt:max(run.eddy.t)]);
-            tinds = cut_nan(vecfind(run.ndtime, 85));
+            %tinds = cut_nan(vecfind(run.ndtime, 85));
+            [~,tinds] = run.calc_maxflux(2);
         else
             tinds = [];
         end
