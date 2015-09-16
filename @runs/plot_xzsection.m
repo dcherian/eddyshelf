cdd7b6f035d6b3742344ee049fe1d9810e51075f
deepak <dpak.cherian@gmail.com>
2015-09-16 11:36:02 -0400

    major upgrades to plot_xzsection

--- @runs/plot_xzsection.m
+++ @runs/plot_xzsection.m
@@ -1,32 +1,102 @@
-function [] = plot_xzsection(runs, days)
+function [] = plot_xzsection(runs, days, hfig)
 
-    tind = vecfind(runs.time, days*86400);
-    yind = runs.bathy.isb + [0 10 20 30];
+    if ~exist('hfig', 'var'), hfig = figure; end
 
-    xr = runs.rgrid.x_v(1,:)/1000;
+    tindex = vecfind(runs.time, days*86400);
+    yind = find_approx(runs.rgrid.y_rho(:,1), ...
+                       runs.eddy.my(tindex)');
 
-    zmin = min(runs.rgrid.z_v(:,max(yind),1));
-    for ii=1:4
-        figure; maximize;
-        for jj=1:4
-            eddye = dc_roms_read_data(runs.dir, 'v', tind(ii), ...
-                                      {runs.bathy.axis yind(jj) ...
-                                yind(jj)}, [], runs.rgrid, 'his', ...
-                                      'single');
+    L = runs.eddy.rhovor.dia(tindex)/2;
 
-            zr = runs.rgrid.z_r(:, yind(jj), 1);
+    zvec = runs.rgrid.z_r(:, yind+1, 1);
+    xvec = (runs.rgrid.x_rho(1,2:end-1) - runs.eddy.mx(tindex)) ...
+           ./ L;
+    zmin = min(runs.rgrid.z_r(:,max(yind),1));
 
-            subplot(2,2,jj)
-            pcolorcen(xr, zr, eddye');
-            hold on
-            contour(xr, zr, eddye', [1 1]*runs.eddy_thresh, 'k', ...
-                    'LineWidth', 2);
-            title(['Day = ' num2str(days(ii)) ' | y = ' ...
-                   num2str(runs.rgrid.y_rho(yind(jj),1)/1000) ' ' ...
-                   'km']);
-            ylim([zmin 0]);
-            caxis([-1 1]*0.05);
-            liney(-1*runs.bathy.hsb);
-        end
-    end
+    % copied from csfluxes.m
+    csvel = squeeze(avg1( ...
+        dc_roms_read_data(runs.dir, 'v', tindex, ...
+                          {runs.bathy.axis yind-1 yind}, [], runs.rgrid, ...
+                          'his', 'single'), 2));
+    csvel = csvel(2:end-1,:,:,:);
+
+    % process cross-shelf dye
+    csdye = dc_roms_read_data(runs.dir, runs.csdname, ...
+                              tindex, {runs.bathy.axis yind yind}, ...
+                              [], runs.rgrid, 'his', 'single');
+    csdye = csdye(2:end-1,:,:);
+
+    eddye = dc_roms_read_data(runs.dir, runs.eddname, ...
+                              tindex, {runs.bathy.axis yind yind}, ...
+                              [], runs.rgrid, 'his', 'single');
+    eddye = eddye(2:end-1,:,:);
+
+    rho = dc_roms_read_data(runs.dir, 'rho', ...
+                            tindex, {runs.bathy.axis yind yind}, ...
+                            [], runs.rgrid, 'his', 'single');
+    rho = rho(2:end-1,:,:);
+
+    rback = dc_roms_read_data(runs.dir, 'rho', ...
+                              1, {runs.bathy.axis yind yind}, ...
+                              [], runs.rgrid, 'his', 'single');
+    rho = rho - rback(2:end-1,:,:);
+
+    %mask = fillnan(bsxfun(@times, csdye < runs.csflux.x(isobath), ...
+    %                      runs.csflux.westmask(:,tindex,isobath)),0)';
+
+    figure(hfig)
+    insertAnnotation([runs.name '.plot_xzsection']);
+
+    ax(1) = subplot(221);
+    [~,h1] = contourf(xvec, zvec, csvel', 20);
+    hold on
+    caxis([-1 1]);
+    % contour(xvec, zvec, repnan(mask,0), [1 1], 'k', 'LineWidth', 2);
+    runs.add_timelabel(tindex);
+    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    title(['v (m/s) | ' runs.name]);
+    % linex(xfrac);
+    center_colorbar;
+    xlim([-1 1]*max(abs(xlim))/2);
+
+    ax(2) = subplot(222);
+    pcolorcen(xvec, zvec, csdye'/1000); % .* mask
+    hold on; shading interp
+    % contour(xvec, zvec, repnan(mask,0), [1 1], 'k', 'LineWidth', 2);
+    colorbar;
+    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    title(['Cross-shelf dye (km) | ' runs.name]);
+    runs.add_timelabel(tindex);
+    linkaxes(ax, 'xy');
+    % linex(xfrac);
+    liney(-1 * runs.eddy.Lgauss(tindex), 'vertical scale');
+    liney(-1 * runs.bathy.hsb, 'h_{sb}');
+
+    ax(3) = subplot(223);
+    pcolorcen(xvec, zvec, rho'); % .* mask
+    hold on; shading interp;
+    % contour(xvec, zvec, repnan(mask,0), [1 1], 'k', 'LineWidth', 2);
+    colorbar;
+    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    title(['Cross-shelf dye (km) | ' runs.name]);
+    runs.add_timelabel(tindex);
+    linkaxes(ax, 'xy');
+    % linex(xfrac);
+    liney(-1 * runs.eddy.Lgauss(tindex), 'vertical scale');
+    liney(-1 * runs.bathy.hsb, 'h_{sb}');
+    caxis([-0.05 0]);
+
+    ax(4) = subplot(224);
+    pcolorcen(xvec, zvec, eddye'); % .* mask
+    hold on; shading interp;
+    % contour(xvec, zvec, repnan(mask,0), [1 1], 'k', 'LineWidth', 2);
+    colorbar;
+    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    title(['Eddy dye (km) | ' runs.name]);
+    runs.add_timelabel(tindex);
+    linkaxes(ax, 'xy');
+    % linex(xfrac);
+    liney(-1 * runs.eddy.Lgauss(tindex), 'vertical scale');
+    liney(-1 * runs.bathy.hsb, 'h_{sb}');
+    caxis([0 1]);
 end
