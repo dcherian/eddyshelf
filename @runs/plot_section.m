3b70e0c21cddf9b676aab2baecb6aed4ca351846
deepak <dpak.cherian@gmail.com>
2015-09-18 11:32:29 -0400

    added runs.plot_section

---
+++ @runs/plot_section.m
@@ -0,0 +1,14 @@
+% use mod_movie to generate section plots (SINGLE TIME INSTANT)
+function [] = plot_section(runs,varname, tindex, volume, axis, index)
+
+    figure;
+    mod_movie(runs, varname, tindex, volume, axis, index);
+    insertAnnotation([runs.name '.plot_section']);
+
+    if axis == 'z' || axis == 's'
+        runs.plot_bathy('contour');
+        hrho = runs.plot_rho_contour('contour', tindex);
+        plot(runs.eddy.mx/1000, runs.eddy.my/1000);
+        plot(runs.eddy.mx(tindex)/1000, runs.eddy.my(tindex)/1000, 'kx');
+    end
+end
