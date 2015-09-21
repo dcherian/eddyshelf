2774308ed064948a62745a42de7ebb8460d58010
deepak <dpak.cherian@gmail.com>
2015-09-21 18:46:11 -0400

    add quiver code from animate_pt to animate_zslice

--- @runs/animate_zslice.m
+++ @runs/animate_zslice.m
@@ -3,6 +3,11 @@ function [] = animate_zslice(runs,varname,depth,tind)
     if ~exist('tind','var'), tind = []; end
     [~,tind,~,nt,stride] = roms_tindices(tind,Inf,length(runs.time));
 
+    % quiver options
+    addvelquiver = 1;
+    dxi = 10; dyi = 3;
+    uref = runs.eddy.V(1)/10; vref = uref;
+
     runs.video_init(['z' num2str(depth) '-' varname]);
     if strcmpi(varname,'rv') || strcmpi(varname, 'pv')
         grids = [runs.dir '/ocean_vor.nc'];
@@ -11,11 +16,26 @@ function [] = animate_zslice(runs,varname,depth,tind)
     end
 
     [grd.xax,grd.yax,grd.zax,~] = dc_roms_extract(grids,varname,{},1);
+
+    if addvelquiver
+        grdu.xax = repmat(runs.rgrid.x_u',[1 1 runs.rgrid.N]);
+        grdu.yax = repmat(runs.rgrid.y_u',[1 1 runs.rgrid.N]);
+        grdu.zax = permute(runs.rgrid.z_u,[3 2 1]);
+
+        grdv.xax = repmat(runs.rgrid.x_v',[1 1 runs.rgrid.N]);
+        grdv.yax = repmat(runs.rgrid.y_v',[1 1 runs.rgrid.N]);
+        grdv.zax = permute(runs.rgrid.z_v,[3 2 1]);
+    end
+
     datain = 0;
     if nt < 20
         tic; disp('Reading data...');
-        data = dc_roms_read_data(runs.dir, varname, tind, {}, [], ...
-                                     grids);
+        data = dc_roms_read_data(runs.dir, varname, tind, {}, [], grids);
+        if addvelquiver
+            u1 = dc_roms_read_data(runs.dir, 'u', tind, {}, [], grids);
+            v1 = dc_roms_read_data(runs.dir, 'v', tind, {}, [], grids);
+        end
+
         datain = 1;
         var = nan([size(data,1) size(data,2) nt]);
         toc;
@@ -46,9 +66,21 @@ function [] = animate_zslice(runs,varname,depth,tind)
             end
             disp(['interpolating timestep ' num2str(mmm) '/' num2str(nt)]);
             var(:,:,mmm) = dc_roms_zslice_var(data(:,:,:,mmm),depth,grd);
+            if addvelquiver
+                u(:,:,mmm) = dc_roms_zslice_var(u1(:,:,:,mmm),depth,grdu);
+                v(:,:,mmm) = dc_roms_zslice_var(v1(:,:,:,mmm),depth,grdv);
+            end
         end
     end
-    clear data
+    clear data u1 v1
+
+
+    % get on interior RHO points
+    u = avg1(u(:,2:end-1,:),1);
+    v = avg1(v(2:end-1,:,:),2);
+    % decimate for quiver
+    u = u(1:dxi:end,1:dyi:end,:);
+    v = v(1:dxi:end,1:dyi:end,:);
 
     if strcmpi(varname, 'pv')
         var = log10(var);
@@ -94,10 +126,17 @@ function [] = animate_zslice(runs,varname,depth,tind)
     xlabel('X (km)'); ylabel('Y (km)');
     runs.plot_bathy('contour','k');
 
+    hq = quiver(runs.eddy.xr(1:dxi:end,1:dyi:end)/1000, ...
+                runs.eddy.yr(1:dxi:end,1:dyi:end)/1000, ...
+                u(:,:,tt)./uref, v(:,:,tt)./vref, ...
+                'Color', 'k', 'LineWidth', 2);
+
     for tt=2:nt
         runs.video_update;
         pause(0.1);
         set(hc,'ZData',var(:,:,tt));
+        set(hq,'UData',u(:,:,tt));
+        set(hq,'VData',v(:,:,tt));
         shading flat
         runs.update_rho_contour(he,tind(1) + tt-1);
         hcsd.ZData = runs.csdsurf(:,:,tind(1) + tt-1);
