1990435d4bda2f54cc3454216d4aabfe7df611be
deepak <dpak.cherian@gmail.com>
2015-10-12 14:41:42 -0400

    plot_section learnt to set commands if not provided with some

--- @runs/plot_section.m
+++ @runs/plot_section.m
@@ -1,6 +1,8 @@
 % use mod_movie to generate section plots (SINGLE TIME INSTANT)
 function [] = plot_section(runs,varname, tindex, volume, axis, index, commands)
 
+    if ~exist('commands', 'var'), commands = []; end
+
     mod_movie(runs, varname, tindex, volume, axis, index, commands);
     insertAnnotation([runs.name '.plot_section']);
 
