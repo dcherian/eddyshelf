dfb96aa112556f0fd73dc5d8bbbb83a9137f7080
deepak <dpak.cherian@gmail.com>
2015-10-13 17:22:32 -0400

    Added dye y-z section.

--- comm6-figs.m
+++ comm6-figs.m
@@ -8,3 +8,17 @@ ax(2) = subplot(212);
 ew34.array(2).animate_field('dye_03', ax(2), 290, 1);
 title('Cyclone moving northwards');
 export_fig images/ew-34-cyc-acyc-eddye.png
+
+%% eddye y-z section
+%ew36 = runs('../topoeddy/runew-36/');
+tind = 320; limx = [0 150];
+ew36.plot_yzsection(tind)
+ylim([-400 0]);
+xlim(limx);
+hax = gca;
+hax.YTick = sort([hax.YTick -floor(ew36.bathy.hsb)]);
+suplabel('', 't'); title('Eddy dye');
+text(0.85*limx(2), -1 * ew36.eddy.Lgauss(tind), ...
+         {'vertical','scale'}, 'VerticalAlignment', 'Bottom', ...
+         'HorizontalAlignment','Center');
+export_fig images/ew-36-eddye-yz-320.png
