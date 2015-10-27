0c576334240518776d287a7ff0dcc14e5c93ceb8
deepak <dpak.cherian@gmail.com>
2015-10-27 17:14:28 -0400

    Changes to kink mask.
    Instead of doing semi-circle, use eddy contour at two different
    thresholds and abruptly join them at "zpeakwidth". This makes things
    sensitive to zpeakwidth.

--- @runs/makeStreamerSection.m
+++ @runs/makeStreamerSection.m
@@ -30,17 +30,20 @@ function [v, mask] = makeStreamerSection(runs, isobath, maxloc, V0, L0, Lz0)
     y0oL =  R/L0 * (1 - yoR); % y0/L - used in derivation
     xfrac = sqrt(1 - y0oL^2);
 
+    %y0oL = (runs.eddy.my(maxloc) - runs.csflux.x(isobath))/L0;
+    %if isobath < 3, y0oL = 0; end
     v = -2.3 * V0 * xmat .* exp(-xmat.^2 - y0oL.^2) .* (1-erf(-zmat));
 
     [width, zpeak] = runs.predict_zpeak(isobath, 'use');
     width = abs(width/Lz0); zpeak = abs(zpeak/Lz0);
 
-    kzrad = width/2; % kink radius - z
-    kxrad = kzrad; % kink radius - x
-    x0 = -xfrac-kxrad; -xfrac-kxrad;
-    z0 = -1 * width/3;
+    %kzrad = width/2; % kink radius - z
+    %kxrad = kzrad; % kink radius - x
+    %x0 = -xfrac-kxrad; -xfrac-kxrad;
+    %z0 = -1 * width/3;
     xline = 0; -xfrac;
 
+    a = 2;
     if ~isreal(xfrac)
         % complex xfrac -- cannot be trusted
         % make the kink (semi-circle) intersect the eddy contour
@@ -54,11 +57,9 @@ function [v, mask] = makeStreamerSection(runs, isobath, maxloc, V0, L0, Lz0)
         mask = xmat < 0;
         v = repmat(v(:,1), [1 size(v,2)]);
     else
-        eddymask = ((xmat.^2 + zmat.^2) > 1^2);
-        kinkmask = (((xmat-x0)/kxrad).^2 + ((zmat-z0)/kzrad).^2) <= 1;
-        %if runs.bathy.hsb/Lz0 > 0.4
-        %    kinkmask(:) = 0;
-        %end
+        eddymask = ((xmat.^a + zmat.^a) > 1.0^a) .* (zmat < -width);
+        %kinkmask = (((xmat-x0)/kxrad).^2 + ((zmat-z0)/kzrad).^2) <= 1;
+        kinkmask = ((xmat.^a + zmat.^a) > 0.7^a) .* (zmat > -width);
         mask = (xmat < xline) & (eddymask | kinkmask);
     end
 
