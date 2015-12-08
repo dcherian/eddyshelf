ec93caa52532d19e6de93813d6371459556fe642
deepak <dpak.cherian@gmail.com>
2015-12-08 17:18:19 -0500

    Massive upgrades to plot_xzsection.
    1. Remove sponge regions.
    2. Add vertical velocity
    3. Fixed labels.
    4. Returns handles.

--- @runs/plot_xzsection.m
+++ @runs/plot_xzsection.m
@@ -1,4 +1,6 @@
-function [] = plot_xzsection(runs, loc, day, debug_flux)
+function [handles] = plot_xzsection(runs, loc, day, debug_flux)
+
+    dxi = 5; dzi = 3;
 
     if ~exist('debug_flux', 'var')
         debug_flux = 0; % debug flux parameterization
@@ -34,96 +36,90 @@ function [] = plot_xzsection(runs, loc, day, debug_flux)
             end
         end
     end
-    ix = find_approx(runs.rgrid.y_rho(:,1), loc);
 
-    L = runs.eddy.rhovor.dia(tindex)/2;
-
-    % copied from csfluxes.m
-    if runs.csvelname == 'v'
-        bathyax = 2;
+    if runs.bathy.axis == 'y'
+        sx1 = runs.spng.sx1;
+        sx2 = runs.spng.sx2;
+        ix = find_approx(runs.rgrid.y_rho(:,1), loc);
+        bathyax = 2; asax = 1;
         xvec = runs.rgrid.x_rho(1,2:end-1) - runs.eddy.mx(tindex);
         zvec = runs.rgrid.z_r(:, ix, 1);
     else
-        bathyax = 1;
+        sx1 = runs.spng.sy1;
+        sx2 = runs.spng.sy2;
+        ix = find_approx(runs.rgrid.x_rho(1,:), loc);
+        bathyax = 1; asax = 2;
         xvec = (runs.rgrid.y_rho(2:end-1,1) - runs.eddy.my(tindex))';
         zvec = runs.rgrid.z_r(:, 1, ix);
     end
+
+    xvec = xvec(sx1:sx2);
+    L = runs.eddy.rhovor.dia(tindex)/2;
+    xsb = runs.bathy.xsb;
 
     csvel = squeeze(avg1( ...
         dc_roms_read_data(runs.dir, runs.csvelname, tindex, ...
                           {runs.bathy.axis ix-1 ix}, [], runs.rgrid, ...
                           'his', 'single'), bathyax));
-    csvel = csvel(2:end-1,:,:,:);
+    csvel = csvel(sx1:sx2,:);
+
+    %asvel = squeeze(avg1( ...
+    %    dc_roms_read_data(runs.dir, runs.asvelname, tindex, ...
+    %                      {runs.bathy.axis ix ix}, [], runs.rgrid, ...
+    %                      'his', 'single'), asax));
+    %asvel = asvel(sx1:sx2,:);
+
+    w = squeeze(avg1( ...
+        dc_roms_read_data(runs.dir, 'w', tindex, ...
+                          {runs.bathy.axis ix ix}, [], runs.rgrid, ...
+                          'his', 'single'), 2));
+    w = w(sx1:sx2,:);
 
     % process cross-shelf dye
     csdye = dc_roms_read_data(runs.dir, runs.csdname, ...
                               tindex, {runs.bathy.axis ix+1 ix+1}, ...
                               [], runs.rgrid, 'his', 'single');
-    csdye = csdye(2:end-1,:,:);
+    csdye = csdye(sx1:sx2,:);
 
     eddye = dc_roms_read_data(runs.dir, runs.eddname, ...
                               tindex, {runs.bathy.axis ix+1 ix+1}, ...
                               [], runs.rgrid, 'his', 'single');
-    eddye = eddye(2:end-1,:,:);
+    eddye = eddye(sx1:sx2,:);
 
     rho = dc_roms_read_data(runs.dir, 'rho', ...
                             tindex, {runs.bathy.axis ix+1 ix+1}, ...
                             [], runs.rgrid, 'his', 'single');
-    rho = rho(2:end-1,:,:);
+    rho = rho(sx1:sx2,:);
 
     rback = dc_roms_read_data(runs.dir, 'rho', ...
                               1, {runs.bathy.axis ix+1 ix+1}, ...
                               [], runs.rgrid, 'his', 'single');
-    % rho = rho - rback(2:end-1,:,:);
-
-    mask = fillnan(bsxfun(@times, csdye < runs.csflux.x(isobath), ...
-                          runs.csflux.offmask(:,tindex,isobath)),0);
-
-    % if exist('isobath', 'var') & isobath == 1
-    %     % define a mask based on first zero-crossing at each depth
-    %     csvoffmask = csvel > 0;
-    %     % pick out region with eddy center
-    %     regions = bwconncomp(csvoffmask, 8);
-    %     spongemask = ~runs.sponge(2:end-1,ceil(size(runs.sponge,2)/2));
-    %     imx = runs.eddy.imx(tindex) - 20; %find_approx(xvec, runs.eddy.mx(tindex), 1);
-    %     csvoffmask = 1; % reset!
-    %     for zz=1:regions.NumObjects
-    %         maskreg = zeros(regions.ImageSize);
-    %         maskreg(regions.PixelIdxList{zz}) = 1;
-    %         if any(maskreg(imx,:) == 1)
-    %             disp('Making csvel based mask');
-    %             csvoffmask = bsxfun(@and, ...
-    %                                 bsxfun(@and, maskreg, ...
-    %                                        runs.csflux.offmask(:,tindex,isobath)), ...
-    %                                 spongemask);
-    %             break;
-    %         end
-    %     end
-    % else
-    %     csvoffmask = 1;
-    % end
+    % rho = rho - rback(sx1:sx2,:);
 
     if isobath == 1
-        csvoffmask = runs.csflux.csvoffmask(:,:,tindex);
+        mask = ones(size(csvel));
     else
-        csvoffmask = 1;
+        mask = fillnan(bsxfun(@times, csdye < runs.csflux.x(isobath), ...
+                              runs.csflux.offmask(sx1:sx2,tindex,isobath)),0);
     end
-    mask = csvoffmask;
 
     % profile I am assuming
     [videal, idmask, xmask, zmask] = runs.makeStreamerSection(isobath);
+    videal = videal(sx1:sx2,:);
+    idmask = idmask(sx1:sx2,:);
+    xmask = xmask(sx1:sx2);
 
-    tind = tindex; % FOR PARAMETERISATION
-                   % syms x z;
-    a = 2; % 2 for gaussian
-           % V0 = runs.eddy.V(tind) * 2.33;
-    R = runs.csflux.R;
-    L = median(runs.eddy.rhovor.dia(1:tind))/2;
-    % Lz = runs.eddy.Lgauss(tindex);
-    % H = runs.csflux.h(isobath);
-    yoR = runs.csflux.ndloc(isobath); % y/R - used in csflux
-    y0oL =  R/L * (1 - yoR); % y0/L - used in derivation
-    xfrac = -sqrt(1 - y0oL^a);
+    % tind = tindex; % FOR PARAMETERISATION
+    %                % syms x z;
+    % a = 2; % 2 for gaussian
+    %        % V0 = runs.eddy.V(tind) * 2.33;
+    % R = runs.csflux.R;
+    % L = median(runs.eddy.rhovor.dia(1:tind))/2;
+    % % Lz = runs.eddy.Lgauss(tindex);
+    % % H = runs.csflux.h(isobath);
+    % yoR = runs.csflux.ndloc(isobath); % y/R - used in csflux
+    % y0oL =  R/L * (1 - yoR); % y0/L - used in derivation
+    % xfrac = -sqrt(1 - y0oL^a);
 
     if debug_flux
         % diagnosed flux
@@ -142,91 +138,98 @@ function [] = plot_xzsection(runs, loc, day, debug_flux)
     insertAnnotation([runs.name '.plot_xzsection']);
     maximize;
 
-    ax(1) = subplot(221);
-    [~,h1] = contourf(xvec/1000, zvec, csvel', 20);
-    h1.EdgeColor = 'none';
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    handles.hax(1) = subplot(221);
+    [~,handles.hcsvel] = contourf(xvec/1000, zvec, csvel', 20);
+    handles.hcsvel.EdgeColor = 'none';
     hold on
-    contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
-    runs.add_timelabel(tindex);
-    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
-    title('Cross-shelf velocity (m/s)');
+    [~,handles.hmask(1)] = ...
+        contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
+    handles.htitle(1) = title('Cross-shelf velocity (m/s)');
     %linex(xfrac);
-    liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-          {'vertical scale'; 'h_{sb}'});
-    caxis([-1 1] * max(abs(csvel(:)))); center_colorbar;
+    common(runs, tindex);
+    caxis([-1 1] * max(abs(csvel(:))));
+    handles.hcb(1) = center_colorbar;
+    handles.htime = runs.add_timelabel(tindex);
+    handles.htime.Position = [0.68 0.13];
+    if exist('isobath', 'var')
+        handles.htime.String = [{handles.htime.String;  ...
+                            ['y/R = ' ...
+                            num2str(runs.csflux.ndloc(isobath), '%.2f')]}];
+    end
     beautify;
     if exist('isobath', 'var') & (isobath == 1)
         ylim([min(zvec(:)) 0]);
     end
 
-    xsb = runs.bathy.xsb;
-    ax(2) = subplot(224);
-    pcolorcen(xvec/1000, zvec, runs.sgntamp*(csdye'-xsb)/1000); % .* mask
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    handles.hax(2) = subplot(224);
+    handles.hcsdye = pcolorcen(xvec/1000, zvec, runs.sgntamp*(csdye'-xsb)/1000); % .* mask
     hold on; shading interp
-    contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
-    hcb = colorbar;
-    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    [~,handles.hmask(4)] = ...
+        contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
+    [~,hrho(1)] = contour(xvec/1000, zvec, rho', 30, 'k'); % .* mask
+    handles.hcb(4) = colorbar;
     title('Cross-shelf dye (km)');
-    runs.add_timelabel(tindex);
-    linkaxes(ax, 'xy');
-    %linex(xfrac);
-    liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-          {'vertical scale'; 'h_{sb}'});
+    linkaxes(handles.hax, 'xy');
+    common(runs, tindex);
 
     % muck with colorbar
     cmin = round(min(runs.sgntamp*(csdye(:) - xsb)/1000));
     cmax = round(max(runs.sgntamp*(csdye(:) - xsb)/1000));
-    hcb.TickLabelsMode = 'auto';
-    hcb.TickDirection = 'out';
-    hcb.Limits = [cmin cmax];
-    hcb.Ticks = sort([cmin/2 cmin 0 round((runs.bathy.xsl + xsb)/2000 - xsb/1000) ...
-                 round(runs.params.eddy.cy/1000 - xsb/1000)]);
-    % hcb.TickLabels{1} = ['Shelfbreak - ' num2str(-1*hcb.Ticks(1)) ' km'];
-    hcb.TickLabels{2} = 'Shelf Water';
-    % hcb.TickLabels{3} = 'Shelfbreak';
-    hcb.TickLabels{4} = 'Slope Water';
-    hcb.TickLabels{end} = 'Eddy Water';
+    handles.hcb(4).TickLabelsMode = 'auto';
+    handles.hcb(4).TickDirection = 'out';
+    handles.hcb(4).Limits = [cmin cmax];
+    handles.hcb(4).Ticks = sort([cmin/2 cmin 0 ...
+                        round((runs.bathy.xsl + xsb)/2000 - xsb/1000) ...
+                        round(runs.params.eddy.cy/1000 - xsb/1000)]);
+    % handles.hcb(4).TickLabels{1} = ['Shelfbreak - ' num2str(-1*handles.hcb(4).Ticks(1)) ' km'];
+    handles.hcb(4).TickLabels{2} = 'Shelf Water';
+    % handles.hcb(4).TickLabels{3} = 'Shelfbreak';
+    handles.hcb(4).TickLabels{4} = 'Slope Water';
+    handles.hcb(4).TickLabels{end} = 'Eddy Water';
     beautify;
 
-    ax(3) = subplot(223);
-    contour(xvec/1000, zvec, rho', 30, 'k'); % .* mask
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    handles.hax(3) = subplot(223);
+    [~,hw] = contourf(xvec/1000, zvec, w', 6);
+    hw.EdgeColor = 'none';
     clim = caxis;
-    hold on; shading interp;
-    contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
-    caxis(clim);
-    colorbar;
-    xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+    hold on;
+    [~,hrho(2)] = contour(xvec/1000, zvec, rho', 30, 'k'); % .* mask
+    [~,handles.hmask(3)] = ...
+        contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
+    %handles.hquiv = quiver(xvec(1:dxi:end)/1000, zvec(1:dzi:end), ...
+    %                       asvel(1:dxi:end, 1:dzi:end)'/1000, ...
+    %                       w(1:dxi:end, 1:dzi:end)');
+    caxis(clim); center_colorbar;
+    common(runs, tindex);
     title('\rho (kg/m^3)');
-    runs.add_timelabel(tindex);
-    linkaxes(ax, 'xy');
-    %linex(xfrac);
+    linkaxes(handles.hax, 'xy');
     beautify;
 
+    hrho(2).LevelList = hrho(1).LevelList;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if ~exist('isobath', 'var')
-        ax(4) = subplot(224);
+        handles.hax(4) = subplot(224);
         pcolorcen(xvec/1000, zvec, eddye'); % .* mask
         hold on; shading interp;
         contour(xvec/1000, zvec, repnan(mask,0), [1 1], 'k', 'LineWidth', 2);
         colorbar;
-        xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
         title(['Eddy dye | ' runs.name]);
-        runs.add_timelabel(tindex);
-        linkaxes(ax, 'xy');
-        % linex(xfrac);
-        liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-              {'vertical scale'; 'h_{sb}'});
+        linkaxes(handles.hax, 'xy');
+        common(runs, tindex);
         caxis([0 1]);
         beautify;
     else
-        subplot(222);
+        handles.hax(2) = subplot(222);
         plot(abs(trapz(xvec, repnan(csvel.*mask,0), 1)), zvec);
         title(runs.name);
-        ylabel('Z (m)');
+        common(runs, tindex);
         xlabel('|Transport| (m^2/s)');
-        liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-              {'vertical scale'; 'h_{sb}'});
-        ylim(ax(1).YLim);
-        beautify;
+        ylim(handles.hax(1).YLim);
+        pbaspect([1.3 1 1]);
     end
 
     if debug_flux
@@ -239,11 +242,9 @@ function [] = plot_xzsection(runs, loc, day, debug_flux)
         hold on
         contour(xvec/1000, zvec, repnan(mask',0), [1 1], 'k', 'LineWidth', 2);
         runs.add_timelabel(tindex);
-        xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+        common(runs, tindex);
         title(['Cross-shelf velocity (m/s) | ' runs.name]);
         % linex(xfrac*L/1000, 'xfrac');
-        liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-              {'vertical scale'; 'h_{sb}'});
         caxis([-1 1] * max(abs(csvel(:)))); center_colorbar;
         beautify;
 
@@ -252,11 +253,9 @@ function [] = plot_xzsection(runs, loc, day, debug_flux)
         hold on
         contour(xmask/1000, zmask, repnan(idmask',0), [1 1], 'k', 'LineWidth', 2);
         runs.add_timelabel(tindex);
-        xlabel('(X - X_{eddy})/L_{eddy}'); ylabel('Z (m)');
+        common(runs, tindex);
         title('Idealized');
-        linex(xfrac*L/1000, 'xfrac');
-        liney(-1 * [runs.eddy.Lgauss(tindex) runs.bathy.hsb], ...
-              {'vertical scale'; 'h_{sb}'});
+        % linex(xfrac*L/1000, 'xfrac');
         caxis([-1 1] * max(abs(csvel(:)))); center_colorbar;
         beautify;
 
@@ -269,4 +268,13 @@ function [] = plot_xzsection(runs, loc, day, debug_flux)
 
         linkaxes(ax, 'xy');
     end
+
+    handles.hrho = hrho;
+end
+
+function [] = common(obj, tindex)
+    xlabel('X - X_{eddy} (km)'); ylabel('Z (m)');
+    liney(-1 * [obj.eddy.Lgauss(tindex) obj.bathy.hsb], ...
+          {'vertical scale'; 'h_{sb}'});
+    beautify;
 end
