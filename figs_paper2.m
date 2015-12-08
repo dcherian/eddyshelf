5fef6d29006fea05b9709879e91b914f0887d6f5
deepak <dpak.cherian@gmail.com>
2015-12-08 17:21:12 -0500

    Updates to plots. Added xzsection.

--- figs_paper2.m
+++ figs_paper2.m
@@ -1,10 +1,17 @@
 % describe fluxes
-if ~exist('ew', 'var') | ~strcmpi(ew.name, 'ew-34')
-    ew = runs('../topoeddy/runew-34/');
+if ~exist('ew34', 'var') | ~strcmpi(ew.name, 'ew-34')
+    ew34 = runs('../topoeddy/runew-34/');
 end
-
 factor = 2;
-handles = ew.plot_fluxts(factor, 3, 3);
+isobath = 4;
+
+%% flux diagnostics
+handles = ew34.plot_fluxts(factor, isobath, isobath);
 handles.htitle.String = ['Flux of water above ' num2str(factor) 'H_{sb}'];
 maximize; drawnow;
 export_fig images/paper2/flux-diags.png
+
+%% x-z sections
+handles = ew34.plot_xzsection(isobath, 225);
+maximize; drawnow;
+export_fig images/paper2/ew-34-xzsection.png
