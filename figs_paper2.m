40f1e717a647f86c8a18c6b7592824c02654a261
deepak <dpak.cherian@gmail.com>
2015-12-09 13:04:25 -0500

    Added secondary cyclone plot.

--- figs_paper2.m
+++ figs_paper2.m
@@ -1,5 +1,5 @@
 % describe fluxes
-if ~exist('ew34', 'var') | ~strcmpi(ew.name, 'ew-34')
+if ~exist('ew34', 'var') | ~strcmpi(ew34.name, 'ew-34')
     ew34 = runs('../topoeddy/runew-34/');
 end
 factor = 2;
@@ -17,3 +17,16 @@ correct_ticks('y', [], 3, handles.hax([1 3 4]));
 correct_ticks('y', [], 4, handles.hax([2]));
 drawnow;
 export_fig images/paper2/ew-34-xzsection.png
+
+%% secondary eddy
+if ~exist('ew2360', 'var') | ~strcmpi(ew2360.name, 'ew-2360_wider')
+    ew2360 = runs('../topoeddy/runew-2360_wider/');
+    xx = [343 346 349]';
+    yy = [231 237 231]';
+end
+opt.addvelquiver = 0;
+opt.csdcontours = ew2360.csflux.x(isobath);
+handles = ew2360.secondary_vortices(95, [xx yy], opt);
+correct_ticks('y', [], '198', handles.hax(1));
+handles.hax(1).Title.String = 'Cross shelf dye - X_{sb} (km)';
+export_fig images/paper2/ew-2360-secondary-cyclone.png
