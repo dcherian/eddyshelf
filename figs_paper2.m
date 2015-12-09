64c163391c99f6acd286dc728501dc93a12fcdf1
deepak <dpak.cherian@gmail.com>
2015-12-09 14:34:06 -0500

    Added ew-34-mosaic-zslice.

--- figs_paper2.m
+++ figs_paper2.m
@@ -30,3 +30,23 @@ handles = ew2360.secondary_vortices(95, [xx yy], opt);
 correct_ticks('y', [], '198', handles.hax(1));
 handles.hax(1).Title.String = 'Cross shelf dye - X_{sb} (km)';
 export_fig images/paper2/ew-2360-secondary-cyclone.png
+
+%% z-slice
+opt.csdcontours = ew34.csflux.x(isobath);
+handles = ew34.mosaic_zslice('dye_03', 200, [225 235 245 260], opt);
+for ii=1:length(handles)
+    handles(ii).eddsurf.Visible = 'off';
+    handles(1).htext(2).Visible = 'off';
+    handles(ii).rhocont.LineWidth = 2.5;
+    handles(ii).csdsurf.LineWidth = 2.5;
+end
+xlim([200 400]);
+ylim([0 150]);
+handles(3).hax.XTickLabel{end} = '';
+handles(1).htitle.String = 'Eddy dye at z = -200 m | H_{sb} = 50m | Ro = 0.10';
+handles(1).htitle.FontSize = 22;
+handles(1).htitle.FontWeight = 'normal';
+handles(1).supax.Position(4) = 0.87;
+axes(handles(1).hax); correct_ticks('y', '', '0');
+axes(handles(3).hax); correct_ticks('y', '', '0');
+export_fig images/paper2/ew-34-mosaic-zslice.png
