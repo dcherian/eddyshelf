e2227e4f751df187952f4ecc29206d4a4cf5f501
deepak <dpak.cherian@gmail.com>
2015-12-16 18:33:30 -0500

    Change flux_tindices.

--- @runs/flux_tindices.m
+++ @runs/flux_tindices.m
@@ -13,5 +13,5 @@ function [start, stop] = flux_tindices(runs, flux)
     %                   1, 'first');
     % Sep 09, 2015: change from 0.02 because of 3341-2-big
     start = find_approx(abs(itrans), 0.05 * max(abs(itrans)), 1);
-    stop = find_approx(abs(itrans), 0.95 * max(abs(itrans)), 1);
+    stop = find_approx(abs(itrans), 0.90 * max(abs(itrans)), 1);
 end
