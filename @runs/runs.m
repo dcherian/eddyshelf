5ed054ef8bba7235e130b1c9db8567d3989e51ce
deepak <dpak.cherian@gmail.com>
2015-12-16 18:48:28 -0500

    Correct velocity scale too.

--- @runs/runs.m
+++ @runs/runs.m
@@ -1178,7 +1178,7 @@ methods
         if ~exist('tend', 'var'), tend = tstart; end
         if ~exist('nsmth', 'var'), nsmth = 10; end
 
-        V = smooth(hypot(runs.eddy.fitx.V0, runs.eddy.fity.V0), nsmth) / 2.3;
+        V = smooth(hypot(runs.eddy.fitx.V0, runs.eddy.fity.V0), nsmth) / 2.3 / sqrt(2);
         %V = smooth(runs.eddy.rhovor.Vke, nsmth) / 2.3;
         L = smooth(hypot(runs.eddy.fitx.Lrho, runs.eddy.fity.Lrho), nsmth) / sqrt(2);
         Lz = smooth(runs.eddy.Lgauss, nsmth);
