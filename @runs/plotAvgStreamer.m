a675cb25812770b0582572aaa2fdf4c70142e8b8
deepak <dpak.cherian@gmail.com>
2015-12-18 11:58:39 -0500

    Updated plotAvgStreamer.
    1. Return handles
    2. Nicer colorbar location.
    3. Use beautify

--- @runs/plotAvgStreamer.m
+++ @runs/plotAvgStreamer.m
@@ -1,4 +1,4 @@
-function [] = plotAvgStreamer(runs, isobath)
+function [handles] = plotAvgStreamer(runs, isobath)
 
     debug = 0;
     %pint = runs.csflux.off.slopewater.vertitrans(:,isobath,source);
@@ -8,26 +8,34 @@ function [] = plotAvgStreamer(runs, isobath)
 
     hf = figure; maximize();
     insertAnnotation([runs.name '.plotAvgStreamer']);
-    ax1 = subplot(3,3,[4 5 7 8]);
-    pcolorcen(xivec, zvec, vmean');
+    ax(1) = subplot(3,3,[4 5 7 8]);
+    handles.hfield = pcolorcen(xivec, zvec, vmean');
     xlabel('X - X_{eddy} (km)'); ylabel('Z (m)');
-    liney(-runs.bathy.hsb);
-    linex(0);
-    title([runs.name ' | mean streamer velocity | y/R = ' num2str(runs.csflux.ndloc(isobath))]);
+    hl = liney([-runs.bathy.hsb -runs.eddy.Lgauss(1)]);
+    hl{1}.XData(2) = ax(1).XTick(end-1);
+    hl{2}.XData(2) = ax(1).XTick(end-1);
+    hl{3} = linex(0);
+    handles.isolabel = runs.add_isobathlabel(isobath);
+    beautify;
+    title([runs.name ' | mean streamer velocity (m/s)']);
     hcb = center_colorbar;
-    hcb.Position(1) = 0.5;
+    hcb.Position(1) = 0.58;
 
-    ax2 = subplot(3,3,[1 2]);
+    ax(2) = subplot(3,3,[1 2]);
     hx = plot(xivec, runs.streamer.off.xprof(:,isobath));
     title('\int dz');
     hold on;
-    linex(0); liney(0);
+    hl2(1) = linex(0); hl2(2) = liney(0);
+    uistack(hl2, 'bottom');
+    beautify;
     ylabel('Flux (m^2/s)');
 
-    ax3 = subplot(3,3,[6 9]);
+    ax(3) = subplot(3,3,[6 9]);
     hz = plot(runs.streamer.off.zprof(:,isobath), zvec);
     title('Offshore transport (\int dx)');
     xlabel('Flux (m^2/s)');
+    beautify;
+    if isobath == 1, xlim([0 max(xlim)*1.15]); end
 
     if debug
         error('debug plots not fixed yet.');
@@ -42,12 +50,12 @@ function [] = plotAvgStreamer(runs, isobath)
                  diff(exp(-abs(xivec'/Ln).^a))./diff(xivec'/Ln) ...
                  * exp(-y0oL.^2);
 
-        axes(ax2);
+        axes(ax(2));
         hx.YData = hx.YData / max(actualx);
         plot(avg1(xivec), idealx./max(idealx), 'k-');
         ylabel('Flux / max flux');
 
-        axes(ax3);
+        axes(ax(3));
         hold on;
         hz.YData = hz.YData / max(pmean);
         plot(pint./max(pint), zvec);
@@ -56,6 +64,17 @@ function [] = plotAvgStreamer(runs, isobath)
         xlabel('Flux / max flux');
     end
 
-    linkaxes([ax1 ax2], 'x');
-    linkaxes([ax1 ax3], 'y');
+    linkaxes(ax(1:2), 'x');
+    linkaxes(ax([1 3]), 'y');
+
+    linkprop(ax(1:2), 'XTick');
+    linkprop(ax([1 3]), 'YTick');
+
+    handles.ax = ax;
+    handles.hx = hx;
+    handles.hz = hz;
+    handles.hcb = hcb;
+
+    handles.ax(2).XTickLabel = {};
+    handles.ax(3).YTickLabel = {};
 end
