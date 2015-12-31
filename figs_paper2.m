061ea1afa350408116b159617c42952cbe8b7d57
deepak <dpak.cherian@gmail.com>
2015-12-31 16:34:11 -0500

    Fix comments.

--- figs_paper2.m
+++ figs_paper2.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% field map
+%% field map
 
 if ~exist('ew34', 'var') | ~strcmpi(ew34.name, 'ew-34')
     ew34 = runs('../topoeddy/runew-34/');
@@ -38,7 +38,7 @@ icons(end).Children.Children(3).LineWidth = 1;
 export_fig -a1 images/paper2/ew-34-surface-csdye.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% center tracks
+%% center tracks
 
 set(groot,'DefaultAxesColorOrder', cbrewer('qual', 'Paired', 8));
 if ~exist('ew', 'var')
@@ -55,7 +55,7 @@ columnlegend(3, names, 'Location', 'NorthWest');
 export_fig -a1 -png -pdf images/paper2/centracks
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% field map
+%% field map
 
 opt.csdcontourplot = 1;
 opt.csdcontours = ew34.csflux.x([1 4 8]);
@@ -70,7 +70,7 @@ correct_ticks('y', [], {'50', '100'}, handles.hax([1 4]));
 export_fig -a1 images/paper2/ew-34-surface-csdye.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% flux diagnostics
+%% flux diagnostics
 
 handles = ew34.plot_fluxts(factor, 3, 3);
 handles.htitle.String = ['Flux of water for z > ' num2str(factor) ' H_{sb}'];
@@ -98,7 +98,8 @@ handles.icons(10).FaceAlpha = 0.2;
 axes(handles.hax(2)); % make visible
 export_fig -a3 images/paper2/flux-diags.png
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% surface map and instantaneous flux
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% surface map and instantaneous flux
 
 folders = {'ew-4341', 'ew-34', 'ew-2341_wider'};
 if ~exist('ewflux', 'var'), ewflux = runArray(folders); end
@@ -165,7 +166,7 @@ hcb.Label.String = 'Cross shelf dye - Y_{sb} (km)';
 export_fig -a1 images/paper2/inst-flux.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% x-z sections
+%% x-z sections
 
 handles = ew34.plot_xzsection(isobath, 225);
 correct_ticks('y', [], 3, handles.hax([1 3 4]));
@@ -174,7 +175,7 @@ drawnow;
 export_fig -a1 images/paper2/ew-34-xzsection.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% secondary eddy - 2360
+%% secondary eddy - 2360
 
 if ~exist('ew2360', 'var') | ~strcmpi(ew2360.name, 'ew-2360_wider')
     ew2360 = runs('../topoeddy/runew-2360_wider/');
@@ -189,7 +190,7 @@ handles.hax(1).Title.String = 'Cross shelf dye - X_{sb} (km)';
 export_fig -a1 images/paper2/ew-2360-secondary-cyclone.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% secondary eddy - ew-34
+%% secondary eddy - ew-34
 
 if ~exist('ew34', 'var')
     ew34 = runs('../topoeddy/runew-34/');
@@ -261,7 +262,7 @@ axes(handles(5).hax); correct_ticks('x', '', '400');
 export_fig -a1 images/paper2/ew-34-mosaic-zslice.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% ew-2360 z-slice mosaic
+%% ew-2360 z-slice mosaic
 
 handles = ew2360.mosaic_zslice('dye_03', 200, [63 70 77 95]);
 for ii=1:length(handles)
@@ -284,7 +285,7 @@ axes(handles(3).hax); correct_ticks('y', '', {'150' '200'});
 export_fig -a1 images/paper2/ew-2360-mosaic-zslice.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% bottom rho PV
+%% bottom rho PV
 
 hplt = ew2360.plotBottomRhoPV(70);
 hplt.h_bathy{1}.LineStyle = '--';
@@ -300,12 +301,10 @@ set(gca, 'YTickMode', 'auto');
 export_fig -a1 images/paper2/ew-2360-bottomrhopv.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% secondary cyclone x-z section
+%% secondary cyclone x-z section
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% shelf + friction flux time series
-
-%% shfric
+%% flux summary
 if ~exist('shfric', 'var')
     folders = { ...
         'runew-34', 'runew-5341', 'runew-8341', ...
@@ -322,7 +321,6 @@ if ~exist('shfric', 'var')
     shfric = runArray(folders, names);
 end
 
-%% flux summary
 handles = shfric.PlotFluxSummary(1);
 axes(handles.hax(1))
 ylim([0 15]);
@@ -386,20 +384,24 @@ uistack(handles2.hplted(3), 'top');
 export_fig -a3 images/paper2/sb-flux-summary.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% avg streamer profiles - shelfbreak
+%% avg streamer profiles - shelfbreak
 
 handles = ew34.plotAvgStreamer(1);
 correct_ticks('y', [], '-50');
 handles.ax(1).Title.String = 'Mean cross-isobath velocity (m/s)';
 export_fig -a1 images/paper2/ew34-avgstreamer-sb.png
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% avg streamer profiles - offshore
+
 handles = ew34.plotAvgStreamer(4);
 handles.ax(1).Title.String = 'Mean cross-isobath velocity (m/s)';
 correct_ticks('y', [], {'-50'; '-400'}, handles.ax([1 3]));
 export_fig -a1 images/paper2/ew34-avgstreamer-sl.png
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% flux vertical profiles
+
 clear handles;
 if ~exist('fluxvert', 'var')
     fluxvert = runArray({'runew-2360_wider', 'runew-34-cyc'});
@@ -458,7 +460,7 @@ end
 export_fig -a3 images/paper2/fluxvertprofile.png
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% shelfbreak flow surface snapshot
+%% shelfbreak flow surface snapshot
 
 if ~exist('ew04', 'var')
     ew04 = runArray({'runew-04', 'runew-8041'});
