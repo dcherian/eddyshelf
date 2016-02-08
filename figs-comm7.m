71fe9cd8c72083bedabc953084028acb5227a086
deepak <dpak.cherian@gmail.com>
2016-02-08 11:29:15 -0500

    Update comm-7

--- figs-comm7.m
+++ figs-comm7.m
@@ -9,8 +9,26 @@ opt.addcsdye = 1;
 opt.csfluxplot = 1;
 opt.csfluxIsobath = 1;
 opt.csdcontours = [97.6 120]*1e3;
-opt.limy = [0 300];
+opt.limy = [0 350];
 
 ew4341.makeVideo = 1;
 
-ew4341.animate_field('eddye', [], 179, 1, opt);
+ew4341.animate_field('rho', [], 1, Inf, opt);
+
+%%
+if ~exist('ew3341', 'var')
+    ew3341 = runs('../topoeddy/runew-3341-2-big/');
+    ew3341.read_csdsurf;
+    ew3341.read_eddsurf;
+    ew3341.read_rhosurf;
+end
+
+opt.addcsdye = 1;
+opt.csfluxplot = 1;
+opt.csfluxIsobath = 1;
+opt.csdcontours = [97.6 120]*1e3;
+opt.limy = [0 350];
+
+ew3341.makeVideo = 1;
+
+ew3341.animate_field('rho', [], 340, 1, opt);
