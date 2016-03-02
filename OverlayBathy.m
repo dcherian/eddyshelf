e9e286923d9603c80c13a7314ead26c8af631ac4
deepak <dpak.cherian@gmail.com>
2016-03-02 16:34:41 -0500

    Finalized overlay-bathy.
    Seems to work. Overlay non-transparent version and check that grid ticks
    line up

--- OverlayBathy.m
+++ OverlayBathy.m
@@ -3,12 +3,28 @@
 load 'data/etopo2_extract.mat'
 
 %%
-ix = find_approx(topo.x, -76, 1):find_approx(topo.x,-66,1);
-iy = find_approx(topo.y, 34, 1):find_approx(topo.y, 44, 1);
+ix = find_approx(topo.x, -76, 1):find_approx(topo.x,-65,1);
+iy = find_approx(topo.y, 35, 1):find_approx(topo.y, 44, 1);
 
-contour(topo.x(ix), topo.y(iy), topo.z(ix,iy)',[0 0], 'k', 'LineWidth', 2);
+hf = figure; hax = gca; maximize;
+insertAnnotation('OverlayBathy.m');
+%m_proj('mercator','lon', [min(topo.x(ix)) max(topo.x(ix))], ...
+%       'lat',[min(topo.y(iy)) max(topo.y(iy))]);
+contour(topo.x(ix), topo.y(iy), topo.z(ix,iy)',[0 -100], 'k', 'LineWidth', 1);
 hold on
-[cc,hh] = contour(topo.x(ix), topo.y(iy), topo.z(ix,iy)',-[100 200 2000], ...
-    'k' , 'LineWidth', 1);
-clabel(cc,hh);
-axis image;
+[~,hc] = contour(topo.x(ix), topo.y(iy), topo.z(ix,iy)',-[50 80 2000 4000 5000], ...
+                 'Color', [1 1 1]*0.35 , 'LineWidth', 1);
+%clabel(cc,hh);
+hax.Units = 'pixels';
+hax.Position = [50 50 747 792]-4;
+hax.XTickLabels = {};
+hax.YTickLabels = {};
+
+%export_fig -painters images/overlay-bathy-non-transparent.png
+hax.Visible = 'off';
+export_fig -painters -transparent images/overlay-bathy.png
+
+% off left = (80,30)
+% bottom right = (825, 820);
+% composite overlay-bathy.png wcr-avhrr-label.png -geometry +79+29 avhrr.png
+% non-transparent version is actually smaller by 2 pixels?! +80+30 works ok
