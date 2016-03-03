fd191a3cd554523881281af3aad35747a9b3c51e
deepak <dpak.cherian@gmail.com>
2016-03-03 13:19:47 -0500

    Add shallow deep comparison

--- figs.m
+++ figs.m
@@ -1,3 +1,32 @@
+%%
+ewshdp = runArray({'ew-64461-9-shallow', 'ew-64461-9-deep', ...
+                   'ew-64361-shallow', 'ew-64361-deep'});
+
+figure; maximize;
+ax(1) = subplot(121);
+ewshdp.filter = [1 2];
+ewshdp.plot_ts('eddy.hcen', ax(1))
+pbaspect([1.618 1 1]);
+title('Rh = 20')
+legend('off');
+
+ax(2) = subplot(122);
+ewshdp.filter = [3 4];
+ewshdp.plot_ts('eddy.hcen', ax(2));
+pbaspect([1.618 1 1]);
+title('Rh = 60');
+legend('off');
+
+linkaxes(ax, 'y');
+packfig(1,2, 'columns');
+ax(2).XTick(1) = [];
+
+[ax(3), htitle] = suplabel('Water depth at eddy center', 't');
+ax(3).Position(4) = 0.72;
+beautify;
+
+export_fig -transparent images/shallow-deep-hcen.png
+
 %% ew-34 vertprofile
 % density profile changes with time as it adjusts to no-flux boundary condition. For some
 % runs (ew-34 etc). this is on the order of Δρ seen due to up/downwelling in cyclone. So, ignore!!!
@@ -23,3 +52,6 @@ ax(2) = subplot(122);
 ew2360.plot_vertProfile('rho', 35, 345, [1 100 200 300 350], ax(2));
 ax(2).Title.String = ['Just outside sponge | ' ax(2).Title.String];
 export_fig -a1 images/ew-2360-vert-profile-rho.png
+
+%% ew-8383 along-shelf velocity
+sh.plot_avgProfile('ubar', 'x', 'res', 1, []);
