70033b2777e1dff4f1f58c98d6608bfdd6ae7e91
deepak <dpak.cherian@gmail.com>
2016-04-11 14:11:00 -0400

    Massive improvements to FitCenterVelocity
    1. Some special casing.
    2. cvy0 can be off if the eddy is moving up and down at the end. Set it
    to zero if it is too much. This gets rid of some special cases.
    3. Average less to be more conservative.

--- @runs/FitCenterVelocity.m
+++ @runs/FitCenterVelocity.m
@@ -6,26 +6,64 @@ function [itfit, tfit, T, BadFitFlag] = FitCenterVelocity(runs, debug)
 
     sgn = runs.sgntamp * sign(runs.params.phys.f0);
     % eddy center (SSH max) velocity
-    cvy = sgn* runs.smoothCenterVelocity([], 'max');
+    cvy = sgn* runs.smoothCenterVelocity(5, 'max');
 
     [~,imin] = min(cvy);
     imin = imin;
     tvec = runs.eddy.t*86400;
 
+    [tvec, uind] = unique(tvec);
+    cvy = cvy(uind);
+
+    % OK default choice.
+    % For some special cases (below), I set this to zero to get better results.
+    % I think if the constant is ≳ 20% of max amplitude,
+    % then the fit is hard to get right
     cvy0 = cvy(end);
+    if abs(cvy0./cvy(imin)) > 0.1, cvy0 = 0; end
 
-    if strcmpi(runs.name, 'ew-56341-2'), cvy0 = 0; end
+    if strcmpi(runs.name, 'ew-64461-4') | strcmpi(runs.name, 'ew-64461-7');
+        % this one appears to split, so cut off the end
+        cvy = cvy(1:220);
+        tvec = tvec(1:220);
+        cvy0 = 0;
+    end
 
-    [v0, T, t0, v1, exitflag, conf] = gauss_fit(tvec(imin:end) - tvec(imin), ...
-                                                (cvy(imin:end) - cvy0)./cvy(imin), ...
+    if strcmpi(runs.name, 'ew-64461-5')
+        % gets to sponge?
+        cvy = cvy(1:250);
+        tvec = tvec(1:250);
+        cvy0 = 0;
+    end
+
+    if strcmpi(runs.name, 'ew-6341-2')
+        % not long enouhg. large oscillations at the end
+        % fit to whole record to constrain
+        imin = 11;
+    end
+    %if strcmpi(runs.name, 'ew-6341-4')
+        %cvy = smooth(cvy, 10);
+        %end
+
+    %if strcmpi(runs.name, 'ew-6362-1')
+        % needs extra smoothing
+        % cvy = smooth(cvy, 5);
+    %end
+
+    %if strcmpi(runs.name, 'ew-56341-2'), cvy0 = 0; end
+
+    [v0, T, t0, v1, exitflag, conf] = gauss_fit(tvec(imin-10:end) - tvec(imin), ...
+                                                (cvy(imin-10:end) - cvy0)./cvy(imin), ...
                                                 debug);
     Tconf = conf(:,2);
 
     % ASSUMES THAT ALL VELOCITY POINTS ARE INDEPENDENT ESTIMATES
     T = [T Tconf(1) Tconf(2)];
     tfit = T + tvec(imin) + t0;
-    itfit = vecfind(tvec, tfit);
+    itfit = vecfind(runs.eddy.t*86400, tfit);
     BadFitFlag = ~exitflag;
+
+    if any(isnan(itfit)), BadFitFlag = 1; end
 
     if debug
         hax = gca;
