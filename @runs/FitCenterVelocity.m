b00529de859e668a75c8bfaea7e63df414a4d4bf
deepak <dpak.cherian@gmail.com>
2016-04-11 16:23:58 -0400

    FitCenterVelocity: More special casing.
    Better debug plots.

--- @runs/FitCenterVelocity.m
+++ @runs/FitCenterVelocity.m
@@ -22,6 +22,10 @@ function [itfit, tfit, T, BadFitFlag] = FitCenterVelocity(runs, debug)
     cvy0 = cvy(end);
     if abs(cvy0./cvy(imin)) > 0.1, cvy0 = 0; end
 
+    % I want to use a few values before the peak as a constraint.
+    % Sometimes this won't work if you don't average the velocity enough.
+    if imin-10 < 1, imin = 11; end
+
     if strcmpi(runs.name, 'ew-64461-4') | strcmpi(runs.name, 'ew-64461-7');
         % this one appears to split, so cut off the end
         cvy = cvy(1:220);
@@ -36,22 +40,15 @@ function [itfit, tfit, T, BadFitFlag] = FitCenterVelocity(runs, debug)
         cvy0 = 0;
     end
 
-    if strcmpi(runs.name, 'ew-6341-2')
-        % not long enouhg. large oscillations at the end
-        % fit to whole record to constrain
-        imin = 11;
-    end
     %if strcmpi(runs.name, 'ew-6341-4')
         %cvy = smooth(cvy, 10);
         %end
 
-    %if strcmpi(runs.name, 'ew-6362-1')
-        % needs extra smoothing
-        % cvy = smooth(cvy, 5);
-    %end
+    if strcmpi(runs.name, 'ew-6441')
+        imin = 51;
+    end
 
     %if strcmpi(runs.name, 'ew-56341-2'), cvy0 = 0; end
-
     [v0, T, t0, v1, exitflag, conf] = gauss_fit(tvec(imin-10:end) - tvec(imin), ...
                                                 (cvy(imin-10:end) - cvy0)./cvy(imin), ...
                                                 debug);
@@ -71,6 +68,6 @@ function [itfit, tfit, T, BadFitFlag] = FitCenterVelocity(runs, debug)
         linex(T); title(runs.name);
         subplot(2,1,2);
         plot(tvec, cvy);
-        linex(tfit);
+        linex([tfit t0]); liney(0);
     end
 end
