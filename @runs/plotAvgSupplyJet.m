56b7e483414f906c85aea3bbf0bf2425fa148175
deepak <dpak.cherian@gmail.com>
2016-04-16 16:45:46 -0400

    Much improved avgSupplyJet plot.

--- @runs/plotAvgSupplyJet.m
+++ @runs/plotAvgSupplyJet.m
@@ -3,28 +3,47 @@ function [] = plotAvgSupplyJet(runs)
     supply = runs.supply;
 
     figure;
+    hax = packfig(3,1);
     insertAnnotation([runs.name '.plotAvgSupplyJet']);
-    subplot(211);
+
+    axes(hax(1));
+    plot(supply.ymat(:,1)/1000 - runs.bathy.xsb/1000, supply.asvint);
+    ylabel({'Depth-integrated'; 'velocity'});
+    title(['Averaged along-shelf supply jet (m/s) | ' runs.name]);
+    beautify([22 24 28]);
+    linex([supply.xmin supply.xmin-supply.xscale]/1000 - runs.bathy.xsb/1000);
+    hax(1).XTickLabel = [];
+
+    axes(hax(2));
     contourf(supply.ymat/1000 - runs.bathy.xsb/1000, supply.zmat, supply.asvmean, ...
             40, 'EdgeColor', 'none');
-    center_colorbar;
-    linex([supply.xmin supply.xmin-supply.xscale]/1000 - runs.bathy.xsb/1000)
-    title(['Averaged along-shelf supply jet (m/s) | ' runs.name]);
+    hcb(1) = center_colorbar;
+    linex([supply.xmin supply.xmin-supply.xscale]/1000 - runs.bathy.xsb/1000);
+    ylabel('Z (m)');
+    %pbaspect([1.618 1 1]);
+    beautify([22 24 28]);
+    hax(2).XTickLabel = [];
+
+    axes(hax(3));
+    contourf(supply.ymat/1000 - runs.bathy.xsb/1000, supply.zmat, ...
+             supply.csdmean/1000 - runs.bathy.xsb/1000, 40, 'EdgeColor', 'none');
+    hold on;
+    contour(supply.ymat/1000 - runs.bathy.xsb/1000, supply.zmat, ...
+            supply.csdmean/1000 - runs.bathy.xsb/1000, [1 1] * 0, 'k', ...
+            'LineWidth', 2);
+    hcb(2) = center_colorbar;
+    text(0.1, 0.1, 'Averaged cross-shelf dye - Y_{sb} (km)', 'Units', 'normalized');
     xlabel('Y - Y_{sb} (km)');
     ylabel('Z (m)');
-    set(gcf, 'Position', [455 387 1013 710]);
-    pbaspect([1.618 1 1]);
+    linex([supply.xmin supply.xmin-supply.xscale]/1000 - runs.bathy.xsb/1000);
+    %pbaspect([1.618 1 1]);
     beautify([22 24 28]);
 
-    subplot(212)
-    contourf(supply.ymat/1000 - runs.bathy.xsb/1000, supply.zmat, supply.csdmean, ...
-            40, 'EdgeColor', 'none');
-    caxis([0 runs.bathy.xsl]);
-    title(['Dye | ' runs.name]);
-    xlabel('Y - Y_{sb} (km)');
-    ylabel('Z (m)');
-    set(gcf, 'Position', [455 387 1013 710]);
-    pbaspect([1.618 1 1]);
-    beautify([22 24 28]);
+    linkaxes(hax, 'x');
+    xlim([min(supply.ymat(:) - runs.bathy.xsb)/1000 ...
+          max(supply.ymat(:) - runs.bathy.xsb)/1000]);
 
+    set(gcf, 'Position', [455 387 1013 710]);
+    hcb(1).Position(1) = 0.92;
+    hcb(2).Position(1) = 0.92;
 end
