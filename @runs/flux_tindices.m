ece12436268946abc7037011c6b6cdea96b7438a
deepak <dpak.cherian@gmail.com>
2016-05-02 16:05:38 -0400

    flux_tindices takes configurable thresholds

--- @runs/flux_tindices.m
+++ @runs/flux_tindices.m
@@ -1,7 +1,10 @@
 % estimate start and end of flux time series that I will use to
 % calculate diagnostics: max and avg.
+% [start, stop] = flux_tindices(runs, flux, whenstart, whenstop)
+function [start, stop] = flux_tindices(runs, flux, whenstart, whenstop)
 
-function [start, stop] = flux_tindices(runs, flux)
+    if ~exist('whenstart', 'var'), whenstart = 0.05; end
+    if ~exist('whenstop', 'var'), whenstop = 0.90; end
 
     itrans = cumtrapz(flux);
 
@@ -12,6 +15,6 @@ function [start, stop] = flux_tindices(runs, flux)
     %start = ind + find(abs(flux(ind:end)) > 0.25 * max(abs(flux(ind:end))), ...
     %                   1, 'first');
     % Sep 09, 2015: change from 0.02 because of 3341-2-big
-    start = find_approx(abs(itrans), 0.05 * max(abs(itrans)), 1);
-    stop = find_approx(abs(itrans), 0.90 * max(abs(itrans)), 1);
+    start = find_approx(abs(itrans), whenstart * max(abs(itrans)), 1);
+    stop = find_approx(abs(itrans), whenstop * max(abs(itrans)), 1);
 end
