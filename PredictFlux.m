9ea90494f3a63781fffe88361e5687ff029fa04f
deepak <dcherian@mit.edu>
2016-10-04 16:34:35 -0400

    Update PredictFlux with values for all submissions.
    (should've committed before thesis).

--- PredictFlux.m
+++ PredictFlux.m
@@ -8,9 +8,9 @@ function [Flux, FluxError] = PredictFlux(phys,eddy,flux)
 
     if ~exist('eddy', 'var')
         % eddy properties
-        eddy.V0 = 2; % maximum velocity in eddy (m/s)
-        eddy.L0 = 70e3; % horizontal scale (m)
-        eddy.Lz0 = 700; % vertical scale (m)
+        eddy.V0 = 2; 1.2; % maximum velocity in eddy (m/s)
+        eddy.L0 = 70*sqrt(2)*1e3; 70e3; % horizontal scale (m)
+        eddy.Lz0 = 1000; % vertical scale (m)
     end
 
     if ~exist('flux', 'var')
@@ -28,11 +28,18 @@ function [Flux, FluxError] = PredictFlux(phys,eddy,flux)
     Flux = trapz(xvec, trapz(zvec(zind:end), v(:,zind:end) .* mask(:,zind:end), 2), 1);
 
     yoR = [0 0.17 0.33 0.5 0.67 0.83 1 1.17];
+    % submission 1
     %RegressionSlopes = [0.02 0.12 0.16 0.2 0.24 0.26 0.28 0.3];
     %Uncertainty = [0.0 0.01 0.02 0.03 0.03 0.03 0.03 0.03];
-    RegressionSlopes = [0.11  0.1161 0.1628 0.2026 0.2409 0.2600 0.2830 0.3054];
-    Uncertainty = [0.03 0.0213 0.0298 0.0354 0.0369 0.0338 0.0338 0.0359];
+
+    % submission 2
+    %RegressionSlopes = [0.11  0.1161 0.1628 0.2026 0.2409 0.2600 0.2830 0.3054];
+    %Uncertainty = [0.03 0.0213 0.0298 0.0354 0.0369 0.0338 0.0338 0.0359];
     % flat shelf: 0.1394 ± 0.0225
+
+    % submission 3
+    RegressionSlopes =  [0.09 0.1477 0.2061 0.2530 0.3004 0.3274 0.3577 0.3861];
+    Uncertainty = [0.03 0.0264 0.0355 0.0417 0.0434 0.0401 0.0404 0.0422];
 
     Slope = interp1(yoR, RegressionSlopes, flux.IsobathLocation./eddy.L0);
     Error = interp1(yoR, Uncertainty, flux.IsobathLocation./eddy.L0);
