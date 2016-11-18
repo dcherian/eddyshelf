e4b861327667016ccf8623766e6252ed4bc979fe
deepak <dcherian@mit.edu>
2016-11-18 17:45:34 -0500

    Calculate ShelfbreakSSHScale for unmasked mean SSH too.
    Plots (and output) are now more informative.

--- @runs/ShelfbreakSSHScale.m
+++ @runs/ShelfbreakSSHScale.m
@@ -2,7 +2,7 @@
 % 2*sbssh.X should be an along-shelf length scale of shelf water outflow at the shelfbreak
 function [] = ShelfbreakSSHScale(runs)
 
-    [varmean, maskmean,xivec,prof] = runs.avgProfile('zeta', runs.bathy.axis, 'sb', 1);
+    [sshmean, maskmean, xivec, prof] = runs.avgProfile('zeta', runs.bathy.axis, 'sb', 1);
 
     % find interface. do fit of SSH till the interface
     ind = find(maskmean == 1, 1, 'first') - 1;
@@ -10,20 +10,40 @@ function [] = ShelfbreakSSHScale(runs)
         prof = prof./prof(ind)
     end
 
-    prof(ind:end) = prof(ind-1); % flat-line downstream of shelf-eddy water front
-    [y0,X,x0,y1,conf,fitobj] = tanh_fit(xivec(10:end), prof(10:end), 1);
-    title(runs.name);
+    % For mean SSH, let's flatline at the maximum ssh point.
+    sshmeanback = sshmean;
+    [~,imax] = max(sshmean);
+    sshmean(imax+1:end) = sshmean(imax);
+    [y0,X,x0,y1,conf,fitobj] = ...
+        tanh_fit(xivec(10:end), sshmean(10:end), 1);
+    plot(xivec(10:end), sshmeanback(10:end), 'k--');
+    title([runs.name ' | unmasked']);
+
+    sbssh.nomask.X = X;
+    sbssh.nomask.conf = conf(:,2);
+    sbssh.nomask.fitobj = fitobj;
+    sbssh.xvec = xivec;
+    sbssh.nomask.ssh = prof;
+
+    % for masked averaged SSH, flatline downstream of dye front
+    prof(ind:end) = prof(ind-1);
+    [y0,X,x0,y1,conf,fitobj] = ...
+        tanh_fit(xivec(10:end), prof(10:end), 1);
+    plot(xivec(10:end), sshmeanback(10:end), 'k--');
+    title([runs.name ' | masked']);
 
     sbssh.X = X;
     sbssh.conf = conf(:,2);
     sbssh.fitobj = fitobj;
-    sbssh.xvec = xivec;
     sbssh.ssh = prof;
 
     sbssh.hash = githash([mfilename('fullpath') '.m']);
 
     save([runs.dir '/sbssh.mat'], 'sbssh');
 
-    disp(['Shelfbreak ΔSSH scale = ' num2str(abs(X/1000), '%.1f') ' km']);
+    disp(['Shelfbreak ΔSSH scale (unmasked) = ' ...
+          num2str(abs(sbssh.nomask.X/1000), '%.1f') ' km']);
+    disp(['Shelfbreak ΔSSH scale (shelf-water) = ' ...
+          num2str(abs(X/1000), '%.1f') ' km']);
     runs.sbssh = sbssh;
 end
