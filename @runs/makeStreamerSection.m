ea18cc5e8b68673bbef1879965ee3217e9e8b592
deepak <dpak.cherian@gmail.com>
2017-09-13 12:03:11 -0700

    remove a lot of commented out code + sbreak stuff.

--- @runs/makeStreamerSection.m
+++ @runs/makeStreamerSection.m
@@ -3,7 +3,6 @@ function [v, mask, rho, xvec, zvec] = ...
         makeStreamerSection(runs, isobath, maxloc, V0, L0, Lz0)
 
     debug = 0;
-    circle_kink = 0;
 
     phys = runs.params.phys;
 
@@ -41,82 +40,18 @@ function [v, mask, rho, xvec, zvec] = ...
 
     R = runs.csflux.R;
     yoR = runs.csflux.ndloc(isobath); % y/R - used in csflux
-                                      %y0oL =  R/L0 * (1 - yoR); % y0/L - used in derivation
-                              %xfrac = sqrt(1 - y0oL^2);
-    %y0oL = (runs.eddy.my(maxloc) - runs.csflux.x(isobath))/L0;
-
-    % this works - compares ok with runs.eddy.fity.rho0(1)
-    % RhoAmp = -1 * runs.sgntamp * V0 * phys.f0 * phys.R0/phys.g * L0/Lz0;
+                                      % y0oL =  R/L0 * (1 - yoR); % y0/L - used in derivation
+                                      %xfrac = sqrt(1 - y0oL^2);
+                                      %y0oL = (runs.eddy.my(maxloc) - runs.csflux.x(isobath))/L0;
 
     % eddy fields
     a = 2;
     v = -sqrt(2*exp(1)) * runs.sgntamp * V0 * xmat.^(a-1) .* exp(-xmat.^a) .* (1-erf(-zmat));
-    % rho = RhoAmp .* exp(-xmat.^2 - y0oL^a) .* exp(-zmat.^2);
-
-    % % calculate background density profiles
-    % [rhoshelf, zshelf] = runs.getDensityProfile(runs.bathy.isb);
-    % [rhoslope, zslope] = runs.getDensityProfile(runs.csflux.ix(isobath));
-
-    % rhobot = rhoshelf(1);
-    % rhoedge = RhoAmp * exp(-y0oL^a) * exp(-1/2) * exp(-(zslope/Lz0).^2);
-    % zind = find_approx(rhoslope + rhoedge, rhobot, 1);
-    % width = zvec(zind);
-
-    % [width, zpeak] = runs.predict_zpeak(isobath, 'use', maxloc);
-    % width = abs(width/Lz0); % zpeak = abs(zpeak/Lz0);
-
-    % if circle_kink
-    %     kzrad = width/2; % kink radius - z
-    %     kxrad = kzrad; % kink radius - x
-    %     x0 = -xfrac-kxrad; -xfrac-kxrad;
-    %     z0 = -1 * width/3;
-    %     if ~isreal(xfrac)
-    %         % complex xfrac -- cannot be trusted
-    %         % make the kink (semi-circle) intersect the eddy contour
-    %         xfrac = sqrt(1 - (width)^2);
-    %         x0 = -xfrac;
-    %         xline = 0;
-    %     end
-    % end
 
     xline = 0;
 
-    if isobath == 0
-        % if close to shelfbreak use barotropic mask
-        % account for sloping shelf by integrating only
-        % to Rhines length scale (L_β). This needs to be
-        % normalized by L0, of course.
-        % I use x/L = -1 as a reference for where there is almost
-        % no velocity. So, starting at -1, integrate a distance of L_β.
-        % So, mask is x/L < -(1-L_β)
-        % If no shelf slope, L_β is set to 1, so that I integrate over all
-        % offshore flow.
-        if runs.bathy.sl_shelf ~= 0
-            betash = runs.params.phys.f0/runs.bathy.hsb * runs.bathy.sl_shelf;
-            Lbeta = 1.37*sqrt(V0/betash) - 3300;
-
-            Lcorr = 1 - 1.42*runs.rrshelf*pi/L0;
-            %Lcorr = sqrt((1-Lbeta)^2 - 1/2);
-
-            if Lcorr > 1
-                % for gentle slopes, I shouldn't do anything.
-                Lcorr = 1;
-            end
-        else
-            Lcorr = 1;
-        end
-
-        Lcorr = 1;
-        mask = xmat < -(1-Lcorr);
-    else
-        eddymask = ((xmat.^a + zmat.^a) > 1.0^a);
-        % if circle_kink
-        %     kinkmask = (((xmat-x0)/kxrad).^2 + ((zmat-z0)/kzrad).^2) <= 1;
-        % else
-        %     kinkmask = ((xmat.^a + zmat.^a) > (1)^a) .* (zmat >= -width);
-        % end
-        mask = (xmat < xline) & (eddymask);
-    end
+    eddymask = ((xmat.^a + zmat.^a) > 1.0^a);
+    mask = (xmat < xline) & (eddymask);
 
     if debug
         figure;
